40efadd07e88e1f17c3942d3c2a6d02faa905135
Senna46 <Senna46@users.noreply.github.com>
2021-08-03 16:23:44 +0900

    feat: jpyx-2 update add node

---
+++ launch/jpyx-2/gentx-jpyx-2.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# lcnem-a
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from lcnem-a ip a.jpyx.lcnem.net --moniker lcnem-a --identity lcnem-a --node-id "" --pubkey jpyxvalconspub1zcjduepqfqafaz96awchqfyrtzddhuj5t7rug4nllk8yexuvl0yzmee5dt2srkkesq
+
+# lcnem-b
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from lcnem-b ip b.jpyx.lcnem.net --moniker lcnem-b --identity lcnem-b --node-id "" --pubkey jpyxvalconspub1zcjduepq73we20e2l69450m8acplrdlq0w0hshn5pcwzgh8gnvgxey07552svk2lj2
+
+# mano-san genio
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from mano-san-1 ip jpyx.mainnet.validator.genio.neukind.network --moniker genio --identity genio --node-id "" --pubkey jpyxvalconspub1zcjduepqc0qh49vf7gypaj4evzn50dzala5ysh0w0tevgdqldv3nm95d8v6sqvx8fg
+
+# niikura-san-1 zofuku-japan
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from niikura-san-1 ip jpyx.mainnet.validator.zofuku-japan.neukind.network --moniker zofuku-japan --identity zofuku_japan --node-id "" --pubkey jpyxvalconspub1zcjduepqw5qee4h96ferfjfcu4gxkwdtq4zq3tdw5gat4sgvjrs6m8rld2gsm3a59h
+
+# niikura-san-2 zofuku-tokyo
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from niikura-san-2 ip jpyx.mainnet.validator.zofuku-tokyo.neukind.network --moniker zofuku-tokyo --identity zofuku_tokyo --node-id "" --pubkey jpyxvalconspub1zcjduepqd06a5ae7343tv662728nugjl2rhd8gml3upal79n38tp4js299qswr6qhh
+
+# chikako-kurita-1 chikako0903
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from chikako-kurita-1 ip jpyx.mainnet.validator.chikako0930.neukind.network --moniker chikako0903 --identity chikako0903 --node-id "" --pubkey jpyxvalconspub1zcjduepqcx6fccqggvqhyfpx00gkthv49we40pdglae3hkrwhy0q8drv7qjqtlhjfv
+
+# yoichiro-kurita-1 kurata0211
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from yoichiro-kurita-1 ip jpyx.mainnet.validator.kurata0211.neukind.network --moniker kurata0211 --identity kurata0211 --node-id "" --pubkey jpyxvalconspub1zcjduepqsfdtdq2fqedlwkd93pr25al6a2gva0tzlwu60y8nz7hgwmzrah0qqt324h
+
+# takano-san-1 keyplayers01
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from takano-san-1 ip jpyx.mainnet.validator.keyplayers01.neukind.network --moniker keyplayers01 --identity keyplayers01 --node-id "" --pubkey jpyxvalconspub1zcjduepqucxcr7nl5l49dsc04r036ea8vkwhd9grar6xgxxtg4dhz967yh7qdq4p8k
+
+# takano-san-2 keyplayers02
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from takano-san-2 ip jpyx.mainnet.validator.keyplayers02.neukind.network --moniker keyplayers02 --identity keyplayers02 --node-id "" --pubkey jpyxvalconspub1zcjduepqx86xjj20tw7wt0cra9cpyh4zmuka9cx05255k24ypyt97ysdxsqsm2emyt
+
+# tokyo-0
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from tokyo-0 ip jpyx.mainnet.validator.tokyo-0.neukind.network --moniker tokyo-0 --identity tokyo-0 --node-id "" --pubkey jpyxvalconspub1zcjduepq06hef840twnjqeemqqx4mhrtcydm4gcvxdedz5txtf9088s5w5qsjx008n
+
+# tokyo-1
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from tokyo-1 ip jpyx.mainnet.validator.tokyo-1.neukind.network --moniker tokyo-1 --identity tokyo-1 --node-id "" --pubkey jpyxvalconspub1zcjduepq78mjsrtmgt96uze02j4s43w32fcl5ua0vlm0hx08lw4pm88uyuts5uc00w
+
+# tokyo-2
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from tokyo-2 ip jpyx.mainnet.validator.tokyo-2.neukind.network --moniker tokyo-2 --identity tokyo-2 --node-id "" --pubkey jpyxvalconspub1zcjduepqa23wcrkccpaa3dflgq3x0rq3s99ptrwvywhdlgyyt86ulmhm6sqq0mhufy
+
+# tokyo-3
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from tokyo-3 ip jpyx.mainnet.validator.tokyo-3.neukind.network --moniker tokyo-3 --identity tokyo-3 --node-id "" --pubkey jpyxvalconspub1zcjduepqlv0z8epq90stsavujfhnjph9ru30eqqsjj6eeeqa3j5ddmkuazeqqz994q
+
+# tokyo-4
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from tokyo-4 ip jpyx.mainnet.validator.tokyo-4.neukind.network --moniker tokyo-4 --identity tokyo-4 --node-id "" --pubkey jpyxvalconspub1zcjduepqulr7qeykw4rauddf5u5u0vzvc8pxw3ld2e6mcar7yvrtn9nmstpqkfetmh
+
+# add val iijima-san-1 toko1631
+jpyxd gentx --amount 5000000000ujsmn --chain-id jpyx-2 --from iijima-san-1 ip jpyx.mainnet.validator.toko1631.neukind.network --moniker toko1631 --identity toko1631 --node-id "" --pubkey jpyxvalconspub1zcjduepqg0ug5x8xt8y52ydlxqrfw2de8a22mjwj7an9jj2mln5z6qp2h82sn4yeyh
+
+
+# gentx-"hash".json into genesis.json
+jpyxd collect-gentxs
