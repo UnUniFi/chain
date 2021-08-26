a03df826a960ff5d08408bda801b1f3836f59ec3
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-26 15:03:27 +0900

    feat: abci test for jpyx

--- x/auction/abci_test.go
+++ x/auction/abci_test.go
@@ -6,15 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth"
-	authexported "github.com/cosmos/cosmos-sdk/x/auth/exported"
-	"github.com/cosmos/cosmos-sdk/x/supply"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-	abci "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
-	"github.com/kava-labs/kava/app"
-	"github.com/kava-labs/kava/x/auction"
-	"github.com/kava-labs/kava/x/cdp"
+	"github.com/lcnem/jpyx/app"
+	"github.com/lcnem/jpyx/x/auction"
+	auctiontypes "github.com/lcnem/jpyx/x/auction/types"
+	cdptypes "github.com/lcnem/jpyx/x/cdp/types"
 )
 
 func TestKeeper_BeginBlocker(t *testing.T) {
@@ -23,19 +22,24 @@ func TestKeeper_BeginBlocker(t *testing.T) {
 	buyer := addrs[0]
 	returnAddrs := addrs[1:]
 	returnWeights := []sdk.Int{sdk.NewInt(1)}
-	sellerModName := cdp.LiquidatorMacc
+	sellerModName := cdptypes.LiquidatorMacc
+	sellerAddr := authtypes.NewModuleAddress(sellerModName)
 
 	tApp := app.NewTestApp()
-	sellerAcc := supply.NewEmptyModuleAccount(sellerModName)
-	require.NoError(t, sellerAcc.SetCoins(cs(c("token1", 100), c("token2", 100), c("debt", 100))))
+	sk := tApp.GetBankKeeper()
+	ctx := tApp.NewContext(true, tmproto.Header{})
+
+	// sellerAcc := authtypes.NewEmptyModuleAccount(sellerModName)
+	// require.NoError(t, sellerAcc.SetCoins(cs(c("token1", 100), c("token2", 100), c("debt", 100))))
+	require.NoError(t, sk.SetBalances(ctx, sellerAddr, cs(c("token1", 100), c("token2", 100), c("debt", 100))))
 	tApp.InitializeFromGenesisStates(
-		NewAuthGenStateFromAccs(authexported.GenesisAccounts{
-			auth.NewBaseAccount(buyer, cs(c("token1", 100), c("token2", 100)), nil, 0, 0),
-			sellerAcc,
-		}),
+		// NewAuthGenStateFromAccs(authtypes.GenesisAccounts{
+		// 	auth.NewBaseAccount(buyer, cs(c("token1", 100), c("token2", 100)), nil, 0, 0),
+		// 	sellerAcc,
+		// }),
+		app.NewAuthGenState([]sdk.AccAddress{buyer, sellerAddr}, []sdk.Coins{cs(c("token1", 100), c("token2", 100)), sdk.Coins{}}),
 	)
 
-	ctx := tApp.NewContext(true, abci.Header{})
 	keeper := tApp.GetAuctionKeeper()
 
 	// Start an auction and place a bid
@@ -44,7 +48,7 @@ func TestKeeper_BeginBlocker(t *testing.T) {
 	require.NoError(t, keeper.PlaceBid(ctx, auctionID, buyer, c("token2", 30)))
 
 	// Run the beginblocker, simulating a block time 1ns before auction expiry
-	preExpiryTime := ctx.BlockTime().Add(auction.DefaultBidDuration - 1)
+	preExpiryTime := ctx.BlockTime().Add(auctiontypes.DefaultBidDuration - 1)
 	auction.BeginBlocker(ctx.WithBlockTime(preExpiryTime), keeper)
 
 	// Check auction has not been closed yet
@@ -52,7 +56,7 @@ func TestKeeper_BeginBlocker(t *testing.T) {
 	require.True(t, found)
 
 	// Run the endblocker, simulating a block time equal to auction expiry
-	expiryTime := ctx.BlockTime().Add(auction.DefaultBidDuration)
+	expiryTime := ctx.BlockTime().Add(auctiontypes.DefaultBidDuration)
 	auction.BeginBlocker(ctx.WithBlockTime(expiryTime), keeper)
 
 	// Check auction has been closed
@@ -63,7 +67,7 @@ func TestKeeper_BeginBlocker(t *testing.T) {
 func c(denom string, amount int64) sdk.Coin { return sdk.NewInt64Coin(denom, amount) }
 func cs(coins ...sdk.Coin) sdk.Coins        { return sdk.NewCoins(coins...) }
 
-func NewAuthGenStateFromAccs(accounts authexported.GenesisAccounts) app.GenesisState {
-	authGenesis := auth.NewGenesisState(auth.DefaultParams(), accounts)
-	return app.GenesisState{auth.ModuleName: auth.ModuleCdc.MustMarshalJSON(authGenesis)}
+func NewAuthGenStateFromAccs(accounts authtypes.GenesisAccounts) app.GenesisState {
+	authGenesis := authtypes.NewGenesisState(authtypes.DefaultParams(), accounts)
+	return app.GenesisState{authtypes.ModuleName: authtypes.ModuleCdc.MustMarshalJSON(authGenesis)}
 }
