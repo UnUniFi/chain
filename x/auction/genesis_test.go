c12f91e872d731f8d338f568abb9ccd8d48bafaf
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-30 17:23:47 +0900

    feat: genesis_test for jpyx

--- x/auction/genesis_test.go
+++ x/auction/genesis_test.go
@@ -7,62 +7,75 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	abci "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
-	"github.com/kava-labs/kava/app"
-	"github.com/kava-labs/kava/x/auction"
+	"github.com/lcnem/jpyx/app"
+	jpyxtypes "github.com/lcnem/jpyx/types"
+	"github.com/lcnem/jpyx/x/auction"
+	auctiontypes "github.com/lcnem/jpyx/x/auction/types"
 )
 
 var _, testAddrs = app.GeneratePrivKeyAddressPairs(2)
 var testTime = time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
-var testAuction = auction.NewCollateralAuction(
+var testAuction = auctiontypes.NewCollateralAuction(
 	"seller",
 	c("lotdenom", 10),
 	testTime,
 	c("biddenom", 1000),
-	auction.WeightedAddresses{Addresses: testAddrs, Weights: []sdk.Int{sdk.OneInt(), sdk.OneInt()}},
+	auctiontypes.WeightedAddresses{Addresses: jpyxtypes.StringAccAddresses(testAddrs), Weights: []sdk.Int{sdk.OneInt(), sdk.OneInt()}},
 	c("debt", 1000),
-).WithID(3).(auction.GenesisAuction)
+).WithID(3).(auctiontypes.GenesisAuction)
+var testAuctions, _ = auctiontypes.PackGenesisAuctions(auctiontypes.GenesisAuctions{testAuction})
 
 func TestInitGenesis(t *testing.T) {
 	t.Run("valid", func(t *testing.T) {
 		// setup keepers
 		tApp := app.NewTestApp()
+		sk := tApp.GetBankKeeper()
 		keeper := tApp.GetAuctionKeeper()
-		ctx := tApp.NewContext(true, abci.Header{})
+		ctx := tApp.NewContext(true, tmproto.Header{})
 		// setup module account
-		supplyKeeper := tApp.GetSupplyKeeper()
-		moduleAcc := supplyKeeper.GetModuleAccount(ctx, auction.ModuleName)
-		require.NoError(t, moduleAcc.SetCoins(testAuction.GetModuleAccountCoins()))
-		supplyKeeper.SetModuleAccount(ctx, moduleAcc)
+		// supplyKeeper := tApp.GetSupplyKeeper()
+		accountKeeper := tApp.GetAccountKeeper()
+		moduleAddr := authtypes.NewModuleAddress(auctiontypes.ModuleName)
+		moduleAcc := accountKeeper.GetModuleAccount(ctx, auctiontypes.ModuleName)
+		// require.NoError(t, moduleAcc.SetCoins(testAuction.GetModuleAccountCoins()))
+		require.NoError(t, sk.SetBalances(ctx, moduleAddr, testAuction.GetModuleAccountCoins()))
+		// supplyKeeper.SetModuleAccount(ctx, moduleAcc)
+		accountKeeper.SetModuleAccount(ctx, moduleAcc)
 
 		// create genesis
-		gs := auction.NewGenesisState(
+		gs := auctiontypes.NewGenesisState(
 			10,
-			auction.DefaultParams(),
-			auction.GenesisAuctions{testAuction},
+			auctiontypes.DefaultParams(),
+			// auctiontypes.GenesisAuctions{testAuction},
+			testAuctions,
 		)
 
 		// run init
 		require.NotPanics(t, func() {
-			auction.InitGenesis(ctx, keeper, supplyKeeper, gs)
+			// auction.InitGenesis(ctx, keeper, supplyKeeper, gs)
+			auction.InitGenesis(ctx, keeper, accountKeeper, sk, gs)
 		})
 
 		// check state is as expected
 		actualID, err := keeper.GetNextAuctionID(ctx)
 		require.NoError(t, err)
-		require.Equal(t, gs.NextAuctionID, actualID)
+		require.Equal(t, gs.NextAuctionId, actualID)
 
 		require.Equal(t, gs.Params, keeper.GetParams(ctx))
 
 		// TODO is there a nicer way of comparing state?
 		sort.Slice(gs.Auctions, func(i, j int) bool {
-			return gs.Auctions[i].GetID() > gs.Auctions[j].GetID()
+			// Any型からAuction型へUnpackする
+			unpackAuction, _ := auctiontypes.UnpackAuctions(gs.Auctions)
+			return unpackAuction[i].GetID() > unpackAuction[j].GetID()
 		})
 		i := 0
-		keeper.IterateAuctions(ctx, func(a auction.Auction) bool {
+		keeper.IterateAuctions(ctx, func(a auctiontypes.Auction) bool {
 			require.Equal(t, gs.Auctions[i], a)
 			i++
 			return false
@@ -71,36 +84,44 @@ func TestInitGenesis(t *testing.T) {
 	t.Run("invalid (invalid nextAuctionID)", func(t *testing.T) {
 		// setup keepers
 		tApp := app.NewTestApp()
-		ctx := tApp.NewContext(true, abci.Header{})
+		sk := tApp.GetBankKeeper()
+		keeper := tApp.GetAuctionKeeper()
+		ctx := tApp.NewContext(true, tmproto.Header{})
+		accountKeeper := tApp.GetAccountKeeper()
 
 		// create invalid genesis
-		gs := auction.NewGenesisState(
+		gs := auctiontypes.NewGenesisState(
 			0, // next id < testAuction ID
-			auction.DefaultParams(),
-			auction.GenesisAuctions{testAuction},
+			auctiontypes.DefaultParams(),
+			// auctiontypes.GenesisAuctions{testAuction},
+			testAuctions,
 		)
 
 		// check init fails
 		require.Panics(t, func() {
-			auction.InitGenesis(ctx, tApp.GetAuctionKeeper(), tApp.GetSupplyKeeper(), gs)
+			auction.InitGenesis(ctx, keeper, accountKeeper, sk, gs)
 		})
 	})
 	t.Run("invalid (missing mod account coins)", func(t *testing.T) {
 		// setup keepers
 		tApp := app.NewTestApp()
-		ctx := tApp.NewContext(true, abci.Header{})
+		sk := tApp.GetBankKeeper()
+		keeper := tApp.GetAuctionKeeper()
+		ctx := tApp.NewContext(true, tmproto.Header{})
+		accountKeeper := tApp.GetAccountKeeper()
 
 		// create invalid genesis
-		gs := auction.NewGenesisState(
+		gs := auctiontypes.NewGenesisState(
 			10,
-			auction.DefaultParams(),
-			auction.GenesisAuctions{testAuction},
+			auctiontypes.DefaultParams(),
+			// auctiontypes.GenesisAuctions{testAuction},
+			testAuctions,
 		)
 		// invalid as there is no module account setup
 
 		// check init fails
 		require.Panics(t, func() {
-			auction.InitGenesis(ctx, tApp.GetAuctionKeeper(), tApp.GetSupplyKeeper(), gs)
+			auction.InitGenesis(ctx, keeper, accountKeeper, sk, gs)
 		})
 	})
 }
@@ -109,19 +130,20 @@ func TestExportGenesis(t *testing.T) {
 	t.Run("default", func(t *testing.T) {
 		// setup state
 		tApp := app.NewTestApp()
-		ctx := tApp.NewContext(true, abci.Header{})
+		ctx := tApp.NewContext(true, tmproto.Header{})
 		tApp.InitializeFromGenesisStates()
 
 		// export
 		gs := auction.ExportGenesis(ctx, tApp.GetAuctionKeeper())
 
 		// check state matches
-		require.Equal(t, auction.DefaultGenesisState(), gs)
+		// require.Equal(t, auction.DefaultGenesisState(), gs)
+		require.Equal(t, auctiontypes.DefaultGenesis(), gs)
 	})
 	t.Run("one auction", func(t *testing.T) {
 		// setup state
 		tApp := app.NewTestApp()
-		ctx := tApp.NewContext(true, abci.Header{})
+		ctx := tApp.NewContext(true, tmproto.Header{})
 		tApp.InitializeFromGenesisStates()
 		tApp.GetAuctionKeeper().SetAuction(ctx, testAuction)
 
@@ -129,8 +151,9 @@ func TestExportGenesis(t *testing.T) {
 		gs := auction.ExportGenesis(ctx, tApp.GetAuctionKeeper())
 
 		// check state matches
-		expectedGenesisState := auction.DefaultGenesisState()
-		expectedGenesisState.Auctions = append(expectedGenesisState.Auctions, testAuction)
+		// expectedGenesisState := auction.DefaultGenesisState()
+		expectedGenesisState := auctiontypes.DefaultGenesis()
+		expectedGenesisState.Auctions = append(expectedGenesisState.Auctions, testAuctions...)
 		require.Equal(t, expectedGenesisState, gs)
 	})
 }
