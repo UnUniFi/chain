e452e35a4b0cca868db668957254aad72a9cdca9
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-30 18:12:15 +0900

    feat: types genesis test

--- x/auction/types/genesis_test.go
+++ x/auction/types/genesis_test.go
@@ -6,9 +6,16 @@ import (
 	"github.com/stretchr/testify/require"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	auctiontypes "github.com/lcnem/jpyx/x/auction/types"
 )
 
 var testCoin = sdk.NewInt64Coin("test", 20)
+
+// AuctionをAny型からAuction型に変換
+var unpackAuctions, _ = auctiontypes.unpackAuctions(DefaultGenesis().Auctions)
+var unpackSurplusAuction, _ = auctiontypes.unpackAuctions(SurplusAuction)
+var unpackDebtAuction, _ = auctiontypes.unpackAuctions(DebtAuction)
 
 func TestGenesisState_Validate(t *testing.T) {
 	testCases := []struct {
@@ -17,14 +24,18 @@ func TestGenesisState_Validate(t *testing.T) {
 		auctions   GenesisAuctions
 		expectPass bool
 	}{
-		{"default", DefaultGenesisState().NextAuctionID, DefaultGenesisState().Auctions, true},
-		{"invalid next ID", 54, GenesisAuctions{SurplusAuction{BaseAuction{ID: 105}}}, false},
+		// {"default", DefaultGenesis().NextAuctionId, DefaultGenesis().Auctions, true},
+		// {"invalid next ID", 54, GenesisAuctions{SurplusAuction{BaseAuction{ID: 105}}}, false},
+		{"default", DefaultGenesis().NextAuctionId, unpackAuctions, true},
+		{"invalid next ID", 54, GenesisAuctions{unpackSurplusAuction{BaseAuction{Id: 105}}}, false},
 		{
 			"repeated ID",
 			1000,
 			GenesisAuctions{
-				SurplusAuction{BaseAuction{ID: 105}},
-				DebtAuction{BaseAuction{ID: 105}, testCoin},
+				// SurplusAuction{BaseAuction{ID: 105}},
+				// DebtAuction{BaseAuction{ID: 105}, testCoin},
+				unpackSurplusAuction{BaseAuction{Id: 105}},
+				unpackDebtAuction{BaseAuction{Id: 105}, testCoin},
 			},
 			false,
 		},
@@ -32,7 +43,10 @@ func TestGenesisState_Validate(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			gs := NewGenesisState(tc.nextID, DefaultParams(), tc.auctions)
+			// gs := NewGenesisState(tc.nextID, DefaultParams(), tc.auctions)
+			// NewGenesisStateの引数tc.acutionをAny型に変換
+			packAuction, _ := auctiontypes.PackGenesisAuctions(tc.auctions)
+			gs := NewGenesisState(tc.nextID, DefaultParams(), packAuction)
 
 			err := gs.Validate()
 
