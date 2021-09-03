4dea5f4b9761994fd2a76c0995a486b311ca4da5
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-09-03 15:09:35 +0900

    feat: blocked address問題

--- app/app.go
+++ app/app.go
@@ -169,7 +169,8 @@ var (
 
 	// module accounts that are allowed to receive tokens
 	allowedReceivingModAcc = map[string]bool{
-		distrtypes.ModuleName: true,
+		distrtypes.ModuleName:   true,
+		cdptypes.LiquidatorMacc: true,
 	}
 )
 
