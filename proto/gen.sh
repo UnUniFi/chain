af5d939a3006dd317807b663c0abb8d6d557d94c
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-11 12:38:17 +0900

    Update gen.sh

--- proto/gen.sh
+++ proto/gen.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # https://docs.buf.build/installation/
-# go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 # https://docs.buf.build/installation/
+# go install \
+#     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+#     github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+#     google.golang.org/protobuf/cmd/protoc-gen-go \
+#     google.golang.org/grpc/cmd/protoc-gen-go-grpc
 
 set -eo pipefail
 
