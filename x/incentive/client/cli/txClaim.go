1823eabccec2462513b1bb87e2aa43d4cc2ae23e
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-10-21 18:56:50 +0900

    feat: incentive cli

---
+++ x/incentive/client/cli/txClaim.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/lcnem/jpyx/x/incentive/types"
+)
+
+func CmdClaimCdpMintingReward() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "claim-cdp-minting-reward [multiplierName]",
+		Short: "Places a bid",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			multiplierName := args[0]
+
+			msg := types.NewMsgClaimCdpMintingReward(clientCtx.GetFromAddress(), multiplierName)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), &msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
