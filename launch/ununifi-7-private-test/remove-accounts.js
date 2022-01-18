d4d8a54a5d33c3cc7f1be00698f716c387fc0e92
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-01-18 19:51:31 +0900

    fix: remove unnecessary accounts info from genesis

---
+++ launch/ununifi-7-private-test/remove-accounts.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("genesis-pretty.json");
+const json = data.toString("utf-8");
+const obj = JSON.parse(json);
+
+// remove all accounts info
+obj.app_state.auth.accounts.splice(0);
+obj.app_state.bank.balances.splice(0);
+obj.app_state.genutil.gen_txs.splice(0);
+
+fs.writeFileSync("genesis-pretty.json", JSON.stringify(obj, null, "  "));
