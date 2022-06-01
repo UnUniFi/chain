fb50eb84de438d17a79d4191e80d280c34c87e65
jununifi <jun.ununifi@outlook.com>
2022-06-01 10:11:57 +1000

    add keys for storage

--- x/nftmarket/types/keys.go
+++ x/nftmarket/types/keys.go
@@ -1,1 +1,35 @@
 package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "nftmarket"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for nftmarket
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	MemStoreKey = "mem_capability"
+)
+
+const (
+	// nft listing info by nft_id
+	KeyPrefixNftListing = "nft_listing"
+	// nft listing by owner
+	KeyPrefixAddressNftListing = "address_nft_listing"
+	// nft bid by nft_id
+	KeyPrefixNftBid = "nft_bid"
+	// nft bid by owner
+	KeyPrefixAddressBid = "address_bid"
+	// nft loan by nft_id
+	KeyPrefixNftLoan = "nft_loan"
+	// nft loan by owner
+	KeyPrefixAddressLoan = "address_loan"
+	// rewards by address
+	KeyPrefixAddressRewards = "rewards"
+)
