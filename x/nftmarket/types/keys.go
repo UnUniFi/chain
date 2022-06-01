18cd75e48ced6523f8be32d9094d9aa9252b8b5d
jununifi <jun.ununifi@outlook.com>
2022-06-01 13:26:16 +1000

    add prefix utility function to be used for data store

--- x/nftmarket/types/keys.go
+++ x/nftmarket/types/keys.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/address"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "nftmarket"
@@ -29,7 +34,39 @@ const (
 	// nft loan by nft_id
 	KeyPrefixNftLoan = "nft_loan"
 	// nft loan by owner
-	KeyPrefixAddressLoan = "address_loan"
+	KeyPrefixAddressNftLoan = "address_nft_loan"
 	// rewards by address
 	KeyPrefixAddressRewards = "rewards"
 )
+
+func NftBytes(classId, nftId uint64) []byte {
+	return append(sdk.Uint64ToBigEndian(classId), sdk.Uint64ToBigEndian(nftId)...)
+}
+
+func NftListingKey(classId, nftId uint64) []byte {
+	return append([]byte(KeyPrefixNftListing), NftBytes(classId, nftId)...)
+}
+
+func NftAddressNftListingKey(addr sdk.AccAddress, classId, nftId uint64) []byte {
+	return append(append([]byte(KeyPrefixAddressNftListing), address.MustLengthPrefix(addr)...), NftBytes(classId, nftId)...)
+}
+
+func NftBidKey(classId, nftId uint64, bidder sdk.AccAddress) []byte {
+	return append(append([]byte(KeyPrefixNftBid), NftBytes(classId, nftId)...), address.MustLengthPrefix(bidder)...)
+}
+
+func AddressBidKey(classId, nftId uint64, bidder sdk.AccAddress) []byte {
+	return append(append([]byte(KeyPrefixAddressBid), address.MustLengthPrefix(bidder)...), NftBytes(classId, nftId)...)
+}
+
+func NftLoanKey(classId, nftId uint64) []byte {
+	return append([]byte(KeyPrefixNftLoan), NftBytes(classId, nftId)...)
+}
+
+func AddressNftLoanKey(addr sdk.AccAddress, classId, nftId uint64) []byte {
+	return append(append([]byte(KeyPrefixAddressNftLoan), address.MustLengthPrefix(addr)...), NftBytes(classId, nftId)...)
+}
+
+func AddressRewardsKey(addr sdk.AccAddress) []byte {
+	return append([]byte(KeyPrefixAddressRewards), address.MustLengthPrefix(addr)...)
+}
