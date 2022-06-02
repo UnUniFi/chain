3bc5686485cdc2eeb7be450e7a801ade3a1f970f
jununifi <jun.ununifi@outlook.com>
2022-06-03 01:42:37 +1000

    protogen script revert

--- proto/gen.sh
+++ proto/gen.sh
@@ -35,13 +35,13 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 done
 
 # command to generate docs using protoc-gen-doc
-# buf protoc \
-#   -I "proto" \
-#   -I "proto-thirdparty" \
-#   --doc_out=./docs/core \
-#   --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
-#   $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
-# go mod tidy
+buf protoc \
+  -I "proto" \
+  -I "proto-thirdparty" \
+  --doc_out=./docs/core \
+  --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
+  $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
+go mod tidy
 
 # move proto files to the right places
 cp -r github.com/UnUniFi/chain/* ./
