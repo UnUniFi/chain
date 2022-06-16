1667bec0d80c0efaa8ed048dcf3342ff61459a49
jununifi <jun.ununifi@outlook.com>
2022-06-16 09:59:48 +0800

    - resolve merge conflict for auth middleware - ante - add paid amount to bid - implement automatic payment feature - transfer only paid amount when cancelling bid - extend bid if there's bid within gap time

--- x/nftmarket/abci.go
+++ x/nftmarket/abci.go
@@ -14,8 +14,8 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 	params := k.GetParamSet(ctx)
 	listings := k.GetActiveNftListingsEndingAt(ctx, ctx.BlockTime())
 	for _, listing := range listings {
+		bids := k.GetBidsByNft(ctx, listing.NftId.IdBytes())
 		if listing.AutoRelistedCount < params.AutoRelistingCountIfNoBid {
-			bids := k.GetBidsByNft(ctx, listing.NftId.IdBytes())
 			if len(bids) == 0 {
 				listing.EndAt = listing.EndAt.Add(time.Duration(params.NftListingExtendSeconds) * time.Second)
 				listing.AutoRelistedCount++
@@ -34,6 +34,29 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 			if err != nil {
 				fmt.Println(err)
 				continue
+			} else {
+				// automatic payment after listing ends
+				for _, bid := range bids[len(bids)-int(listing.BidActiveRank):] {
+					if bid.AutomaticPayment {
+						bidder, err := sdk.AccAddressFromBech32(bid.Bidder)
+						if err != nil {
+							fmt.Println(err)
+							continue
+						}
+
+						cacheCtx, write := ctx.CacheContext()
+						err = k.PayFullBid(cacheCtx, &types.MsgPayFullBid{
+							Sender: bidder.Bytes(),
+							NftId:  listing.NftId,
+						})
+						if err == nil {
+							write()
+						} else {
+							fmt.Println(err)
+							continue
+						}
+					}
+				}
 			}
 		}
 	}
--- x/nftmarket/keeper/bid.go
+++ x/nftmarket/keeper/bid.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"time"
+
 	"github.com/UnUniFi/chain/x/nftmarket/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -110,30 +112,41 @@ func (k Keeper) PlaceBid(ctx sdk.Context, msg *types.MsgPlaceBid) error {
 	}
 
 	bidder := msg.Sender.AccAddress()
-
-	// TODO: if you are the highest bidder and you want to make a higher bid, bid again
-	// TODO: if the bidder has N hours remaining in the listing when the bidding takes place, the listing time will automatically be extended by n' minutes. (global_option)
-	// TODO: bidders can activate the automatic payment feature at the time of bidding
-
-	// check if bid exists by same bidder on nft
-	if _, err := k.GetBid(ctx, msg.NftId.IdBytes(), bidder); err == nil {
-		return types.ErrBidAlreadyExists
+	increaseAmount := msg.Amount.Amount
+	paidAmount := sdk.ZeroInt()
+	bid, err := k.GetBid(ctx, msg.NftId.IdBytes(), bidder)
+	if err == nil {
+		if bid.Amount.Amount.GTE(msg.Amount.Amount) {
+			return types.ErrInvalidBidAmount
+		}
+		paidAmount = bid.PaidAmount
+		increaseAmount = msg.Amount.Amount.Sub(bid.Amount.Amount)
 	}
 
-	// Add new bid on the listing
-	k.SetBid(ctx, types.NftBid{
-		NftId:  msg.NftId,
-		Bidder: msg.Sender.AccAddress().String(),
-		Amount: msg.Amount,
-	})
-
 	// Transfer required amount of token from bid account to module
-	initialDeposit := msg.Amount.Amount.Quo(sdk.NewInt(int64(listing.BidActiveRank)))
+	initialDeposit := increaseAmount.Quo(sdk.NewInt(int64(listing.BidActiveRank)))
 	if initialDeposit.IsPositive() {
 		err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, bidder, types.ModuleName, sdk.Coins{sdk.NewCoin(listing.BidToken, initialDeposit)})
 		if err != nil {
 			return err
 		}
+	}
+
+	// Add new bid on the listing
+	k.SetBid(ctx, types.NftBid{
+		NftId:            msg.NftId,
+		Bidder:           msg.Sender.AccAddress().String(),
+		Amount:           msg.Amount,
+		AutomaticPayment: msg.AutomaticPayment,
+		PaidAmount:       paidAmount.Add(initialDeposit),
+	})
+
+	// extend bid if there's bid within gap time
+	params := k.GetParamSet(ctx)
+	gapTime := ctx.BlockTime().Add(time.Duration(params.NftListingGapTime) * time.Second)
+	if listing.EndAt.Before(gapTime) {
+		listing.EndAt = gapTime
+		k.SetNftListing(ctx, listing)
 	}
 
 	// Emit event for placing bid
@@ -156,7 +169,14 @@ func (k Keeper) CancelBid(ctx sdk.Context, msg *types.MsgCancelBid) error {
 
 	_ = listing
 
-	// TODO: ensure bid is not winner one
+	// TODO: if you are the only bidder yourself, you cannot cancel
+	// TODO: Bidder can cancel bids free of charge if the bidder's bid rank is below the bid_active_rank.
+	// TODO: if the bid rank is bid_active_rank or higher, the bid can be cancelled by paying a cancellation fee.
+	// TODO: Cancellation Fee Formula: MAX{canceling_bidder's_deposit - (total_deposit - borrowed_lister_amount), 0}
+	// TODO: bids can only be cancelled X days after bidding (global_option)
+	// TODO: tokens will be reimbursed X days after the bid cancellation is approved (global_option)
+	// TODO: Liquidation may occur for sellers whose bids are cancelled.
+	// TODO: the bidder and the bid canceller's Sign must match, otherwise the bid will not be accepted and a log will be kept.
 
 	bidder := msg.Sender.AccAddress()
 
@@ -174,7 +194,7 @@ func (k Keeper) CancelBid(ctx sdk.Context, msg *types.MsgCancelBid) error {
 	// params.NftListingCancelFeePercentage
 
 	// Transfer amount of token to bid account
-	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, bidder, sdk.Coins{bid.Amount})
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, bidder, sdk.Coins{sdk.NewCoin(listing.BidToken, bid.PaidAmount)})
 	if err != nil {
 		return err
 	}
@@ -208,14 +228,17 @@ func (k Keeper) PayFullBid(ctx sdk.Context, msg *types.MsgPayFullBid) error {
 		return types.ErrBidDoesNotExists
 	}
 
-	// params := k.GetParamSet(ctx)
-	// TODO: handle only unpaid amount on bid
-	// Transfer remaining amount of token from bid account
-	err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, bidder, types.ModuleName, sdk.Coins{bid.Amount})
-	if err != nil {
-		return err
-	}
+	// Transfer unpaid amount of token from bid account
+	unpaidAmount := bid.Amount.Amount.Sub(bid.PaidAmount)
+	if unpaidAmount.IsPositive() {
+		err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, bidder, types.ModuleName, sdk.Coins{sdk.NewCoin(listing.BidToken, unpaidAmount)})
+		if err != nil {
+			return err
+		}
 
+		bid.PaidAmount = bid.Amount.Amount
+		k.SetBid(ctx, bid)
+	}
 	// Emit event for paying full bid
 	ctx.EventManager().EmitTypedEvent(&types.EventPayFullBid{
 		Bidder:  msg.Sender.AccAddress().String(),
