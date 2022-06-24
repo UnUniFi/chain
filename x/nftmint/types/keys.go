37df956cf4f07e40fe5c16bdd424eefd10c5976c
taikiFurusyo <taiki.furusyo@gmail.com>
2022-06-24 20:19:29 +0900

    const key definition

---
+++ x/nftmint/types/keys.go
@@ -0,0 +1,20 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "nftmint"
+
+	// StoreKey defines the primary module store key
+	StoreKey = "ununifinftmint"
+
+	// RouterKey is the message route for nftmint
+	RouterKey = ModuleName
+
+	// MemStoreKey defines thee in-memory store key
+	MemStoreKey = "mem_capability"
+)
+
+// const (
+// 	//
+// 	KeyPrefix = "";
+// )
