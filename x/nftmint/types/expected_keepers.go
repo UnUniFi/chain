917b09aaacb2396bcd183b38283ac5b225a711dd
taikiFurusyo <taiki.furusyo@gmail.com>
2022-06-24 19:50:39 +0900

    feat: expected keeper

---
+++ x/nftmint/types/expected_keepers.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	nfttypes "github.com/cosmos/cosmos-sdk/x/nft"
+)
+
+type AccountKeeper interface {
+	GetSequence(ctx sdk.Context, addr sdk.AccAddress) (uint64, error)
+}
+
+type NftKeeper interface {
+	Mint(ctx sdk.Context, token nfttypes.NFT, receiver sdk.AccAddress) error
+	Burn(ctx sdk.Context, classID string, nftID string) error
+
+	Update(ctx sdk.Context, token nfttypes.NFT) error
+	Transfer(ctx sdk.Context, classID string, nftID string, receiver sdk.AccAddress) error
+
+	GetNFT(ctx sdk.Context, classID, nftID string) (nfttypes.NFT, bool)
+	GetNFTsOfClassByOwner(ctx sdk.Context, classID string, owner sdk.AccAddress) (nfts []nfttypes.NFT)
+	GetNFTsOfClass(ctx sdk.Context, classID string) (nfts []nfttypes.NFT)
+	GetOwner(ctx sdk.Context, classID string, nftID string) sdk.AccAddress
+	GetBalance(ctx sdk.Context, classID string, owner sdk.AccAddress) uint64
+	GetTotalSupply(ctx sdk.Context, classID string) uint64
+	HasNFT(ctx sdk.Context, classID, id string) bool
+}
