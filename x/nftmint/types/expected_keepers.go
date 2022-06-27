52e4fe565c3bf55dfe288633d286a251566bafaa
taikiFurusyo <taiki.furusyo@gmail.com>
2022-06-27 19:53:47 +0900

    new interface of Nftkeeper

--- x/nftmint/types/expected_keepers.go
+++ x/nftmint/types/expected_keepers.go
@@ -10,17 +10,21 @@ type AccountKeeper interface {
 }
 
 type NftKeeper interface {
+	NewClass(class nfttypes.Class)
+
 	Mint(ctx sdk.Context, token nfttypes.NFT, receiver sdk.AccAddress) error
 	Burn(ctx sdk.Context, classID string, nftID string) error
-
 	Update(ctx sdk.Context, token nfttypes.NFT) error
 	Transfer(ctx sdk.Context, classID string, nftID string, receiver sdk.AccAddress) error
+
+	GetClass(classid string) nfttypes.Class
+	GetClasses() []nfttypes.Class
 
 	GetNFT(ctx sdk.Context, classID, nftID string) (nfttypes.NFT, bool)
 	GetNFTsOfClassByOwner(ctx sdk.Context, classID string, owner sdk.AccAddress) (nfts []nfttypes.NFT)
 	GetNFTsOfClass(ctx sdk.Context, classID string) (nfts []nfttypes.NFT)
+
 	GetOwner(ctx sdk.Context, classID string, nftID string) sdk.AccAddress
 	GetBalance(ctx sdk.Context, classID string, owner sdk.AccAddress) uint64
 	GetTotalSupply(ctx sdk.Context, classID string) uint64
-	HasNFT(ctx sdk.Context, classID, id string) bool
 }
