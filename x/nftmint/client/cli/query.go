b0b1b52da9a1fff10e8c7401e432ced12167e136
taikiFurusyo <taiki.furusyo@gmail.com>
2022-06-28 19:50:45 +0900

    foundamental files for new module

---
+++ x/nftmint/client/cli/query.go
@@ -0,0 +1,28 @@
+package cli
+
+import (
+	// "context"
+	"fmt"
+
+	"github.com/UnUniFi/chain/x/nftmint/types"
+	"github.com/cosmos/cosmos-sdk/client"
+
+	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand()
+
+	return cmd
+}
