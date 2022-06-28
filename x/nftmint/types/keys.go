9c77233ced5da2f7c694718822af1e07d1a22d8f
taikiFurusyo <taiki.furusyo@gmail.com>
2022-06-28 13:01:27 +0900

    class attributes key prefix and revise val name

--- x/nftmint/types/keys.go
+++ x/nftmint/types/keys.go
@@ -5,16 +5,16 @@ const (
 	ModuleName = "nftmint"
 
 	// StoreKey defines the primary module store key
-	StoreKey = "ununifinftmint"
+	StoreKey = ModuleName
 
 	// RouterKey is the message route for nftmint
 	RouterKey = ModuleName
 
 	// MemStoreKey defines thee in-memory store key
-	MemStoreKey = "mem_capability"
+	MemStoreKey = "mem_nftmint"
 )
 
-// const (
-// 	//
-// 	KeyPrefix = "";
-// )
+const (
+	// class attributes infor by class_id
+	KeyPrefixClassAttributes = "class_attributes"
+)
