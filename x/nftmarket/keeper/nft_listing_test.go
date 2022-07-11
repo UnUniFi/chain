4e5f3b16a860c088bcb50696608f2f763f67e8ff
jununifi <jun.ununifi@outlook.com>
2022-07-11 10:08:33 +0800

    basic functions further testing

--- x/nftmarket/keeper/nft_listing_test.go
+++ x/nftmarket/keeper/nft_listing_test.go
@@ -11,7 +11,10 @@ import (
 // test basic functions of nft listing
 func (suite *KeeperTestSuite) TestNftListingBasics() {
 	owner := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	owner2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
 
+	now := time.Now().UTC()
+	future := now.Add(time.Second)
 	listings := []types.NftListing{
 		{
 			NftId: types.NftIdentifier{
@@ -24,10 +27,95 @@ func (suite *KeeperTestSuite) TestNftListingBasics() {
 			BidToken:           "uguu",
 			MinBid:             sdk.OneInt(),
 			BidActiveRank:      1,
-			StartedAt:          time.Time{},
-			EndAt:              time.Time{},
+			StartedAt:          now,
+			EndAt:              now,
 			FullPaymentEndAt:   time.Time{},
 			SuccessfulBidEndAt: time.Time{},
+			AutoRelistedCount:  0,
+		},
+		{
+			NftId: types.NftIdentifier{
+				ClassId: "1",
+				NftId:   "2",
+			},
+			Owner:              owner.String(),
+			ListingType:        types.ListingType_DIRECT_ASSET_BORROW,
+			State:              types.ListingState_BIDDING,
+			BidToken:           "uguu",
+			MinBid:             sdk.OneInt(),
+			BidActiveRank:      1,
+			StartedAt:          now,
+			EndAt:              now,
+			FullPaymentEndAt:   time.Time{},
+			SuccessfulBidEndAt: time.Time{},
+			AutoRelistedCount:  0,
+		},
+		{
+			NftId: types.NftIdentifier{
+				ClassId: "1",
+				NftId:   "3",
+			},
+			Owner:              owner.String(),
+			ListingType:        types.ListingType_DIRECT_ASSET_BORROW,
+			State:              types.ListingState_END_LISTING,
+			BidToken:           "uguu",
+			MinBid:             sdk.OneInt(),
+			BidActiveRank:      1,
+			StartedAt:          now,
+			EndAt:              now,
+			FullPaymentEndAt:   now,
+			SuccessfulBidEndAt: time.Time{},
+			AutoRelistedCount:  0,
+		},
+		{
+			NftId: types.NftIdentifier{
+				ClassId: "1",
+				NftId:   "4",
+			},
+			Owner:              owner.String(),
+			ListingType:        types.ListingType_DIRECT_ASSET_BORROW,
+			State:              types.ListingState_SELLING_DECISION,
+			BidToken:           "uguu",
+			MinBid:             sdk.OneInt(),
+			BidActiveRank:      1,
+			StartedAt:          time.Time{},
+			EndAt:              now,
+			FullPaymentEndAt:   now,
+			SuccessfulBidEndAt: time.Time{},
+			AutoRelistedCount:  0,
+		},
+		{
+			NftId: types.NftIdentifier{
+				ClassId: "2",
+				NftId:   "1",
+			},
+			Owner:              owner2.String(),
+			ListingType:        types.ListingType_DIRECT_ASSET_BORROW,
+			State:              types.ListingState_SUCCESSFUL_BID,
+			BidToken:           "uguu",
+			MinBid:             sdk.OneInt(),
+			BidActiveRank:      1,
+			StartedAt:          time.Time{},
+			EndAt:              now,
+			FullPaymentEndAt:   now,
+			SuccessfulBidEndAt: now,
+			AutoRelistedCount:  0,
+		},
+		{
+			NftId: types.NftIdentifier{
+				ClassId: "2",
+				NftId:   "2",
+			},
+			Owner:              owner2.String(),
+			ListingType:        types.ListingType_DIRECT_ASSET_BORROW,
+			State:              types.ListingState_LIQUIDATION,
+			BidToken:           "uguu",
+			MinBid:             sdk.OneInt(),
+			BidActiveRank:      1,
+			StartedAt:          time.Time{},
+			EndAt:              now,
+			FullPaymentEndAt:   now,
+			SuccessfulBidEndAt: now,
 			AutoRelistedCount:  0,
 		},
 	}
@@ -42,18 +130,57 @@ func (suite *KeeperTestSuite) TestNftListingBasics() {
 		suite.Require().Equal(listing, gotListing)
 	}
 
+	// check all listings
 	allListings := suite.app.NftmarketKeeper.GetAllNftListings(suite.ctx)
-	suite.Require().Len(allListings, 1)
-	suite.Require().Equal(listings, allListings)
+	suite.Require().Len(allListings, len(listings))
 
-	// TODO: add tests for following functions as well
-	// GetListingsByOwner(ctx sdk.Context, owner sdk.AccAddress) []types.NftListing {
-	// SetNftListing(ctx sdk.Context, listing types.NftListing) {
-	// DeleteNftListing(ctx sdk.Context, listing types.NftListing) {
-	// GetActiveNftListingsEndingAt(ctx sdk.Context, endTime time.Time) []types.NftListing {
-	// GetFullPaymentNftListingsEndingAt(ctx sdk.Context, endTime time.Time) []types.NftListing {
-	// GetSuccessfulBidNftListingsEndingAt(ctx sdk.Context, endTime time.Time) []types.NftListing {
+	// check listing by owner
+	listingsByOwner := suite.app.NftmarketKeeper.GetListingsByOwner(suite.ctx, owner)
+	suite.Require().Len(listingsByOwner, 4)
 
+	// check active listings (bidding or listing status ending now)
+	activeNftListings := suite.app.NftmarketKeeper.GetActiveNftListingsEndingAt(suite.ctx, now)
+	suite.Require().Len(activeNftListings, 0)
+	// check active listings (bidding or listing status ending future time)
+	activeNftListings = suite.app.NftmarketKeeper.GetActiveNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(activeNftListings, 2)
+
+	// full payment listings (sell decision or end listing status ending now)
+	fullPaymentNftListingsEnding := suite.app.NftmarketKeeper.GetFullPaymentNftListingsEndingAt(suite.ctx, now)
+	suite.Require().Len(fullPaymentNftListingsEnding, 0)
+	// full payment listings (sell decision or end listing status ending future)
+	fullPaymentNftListingsEnding = suite.app.NftmarketKeeper.GetFullPaymentNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(fullPaymentNftListingsEnding, 2)
+
+	// successful listing endings (ending now)
+	successfulNftListingsEnding := suite.app.NftmarketKeeper.GetSuccessfulBidNftListingsEndingAt(suite.ctx, now)
+	suite.Require().Len(successfulNftListingsEnding, 0)
+	// successful listing endings (ending future)
+	successfulNftListingsEnding = suite.app.NftmarketKeeper.GetSuccessfulBidNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(successfulNftListingsEnding, 1)
+
+	// delete all the listings
+	for _, listing := range listings {
+		suite.app.NftmarketKeeper.DeleteNftListing(suite.ctx, listing)
+	}
+
+	// check queries after deleting all
+	allListings = suite.app.NftmarketKeeper.GetAllNftListings(suite.ctx)
+	suite.Require().Len(allListings, 0)
+
+	// listings by owner
+	listingsByOwner = suite.app.NftmarketKeeper.GetListingsByOwner(suite.ctx, owner)
+	suite.Require().Len(listingsByOwner, 0)
+
+	// queries for active listings
+	activeNftListings = suite.app.NftmarketKeeper.GetActiveNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(activeNftListings, 0)
+	// queries for full payment queue
+	fullPaymentNftListingsEnding = suite.app.NftmarketKeeper.GetFullPaymentNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(fullPaymentNftListingsEnding, 0)
+	// queries for successful ending queue
+	successfulNftListingsEnding = suite.app.NftmarketKeeper.GetSuccessfulBidNftListingsEndingAt(suite.ctx, future)
+	suite.Require().Len(successfulNftListingsEnding, 0)
 }
 
 // TODO: add test for NftListing following scenario
