08b635a6444074f07092cabdc98fb1ebb5d34a19
jununifi <jun.ununifi@outlook.com>
2022-07-12 17:58:35 +0800

    add test for CancelNftListing

--- x/nftmarket/keeper/nft_listing_test.go
+++ x/nftmarket/keeper/nft_listing_test.go
@@ -6,6 +6,7 @@ import (
 	ununifitypes "github.com/UnUniFi/chain/types"
 	"github.com/UnUniFi/chain/x/nftmarket/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	nfttypes "github.com/cosmos/cosmos-sdk/x/nft"
 	"github.com/tendermint/tendermint/crypto/ed25519"
 )
@@ -341,7 +342,187 @@ func (suite *KeeperTestSuite) TestListNft() {
 	}
 }
 
-// TODO:Add test for CancelNftListing(ctx sdk.Context, msg *types.MsgCancelNftListing) error
+func (suite *KeeperTestSuite) TestCancelNftListing() {
+	acc1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	acc2 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+
+	params := suite.app.NftmarketKeeper.GetParamSet(suite.ctx)
+
+	tests := []struct {
+		testCase     string
+		classId      string
+		nftId        string
+		nftOwner     sdk.AccAddress
+		canceller    sdk.AccAddress
+		cancelAfter  time.Duration
+		numBids      int
+		listBefore   bool
+		endedListing bool
+		expectPass   bool
+	}{
+		{
+			testCase:     "not existing listing",
+			classId:      "class1",
+			nftId:        "nft1",
+			nftOwner:     acc1,
+			canceller:    acc1,
+			cancelAfter:  time.Second * time.Duration(params.NftListingCancelRequiredSeconds+1),
+			numBids:      0,
+			listBefore:   false,
+			endedListing: false,
+			expectPass:   false,
+		},
+		{
+			testCase:     "not owned nft listing",
+			classId:      "class2",
+			nftId:        "nft2",
+			nftOwner:     acc1,
+			canceller:    acc2,
+			cancelAfter:  time.Second * time.Duration(params.NftListingCancelRequiredSeconds+1),
+			numBids:      0,
+			listBefore:   true,
+			endedListing: false,
+			expectPass:   false,
+		},
+		{
+			testCase:     "cancel time not pass",
+			classId:      "class3",
+			nftId:        "nft3",
+			nftOwner:     acc1,
+			canceller:    acc1,
+			cancelAfter:  0,
+			numBids:      0,
+			listBefore:   true,
+			endedListing: false,
+			expectPass:   false,
+		},
+		{
+			testCase:     "already ended listing",
+			classId:      "class4",
+			nftId:        "nft4",
+			nftOwner:     acc1,
+			canceller:    acc1,
+			cancelAfter:  time.Second * time.Duration(params.NftListingCancelRequiredSeconds+1),
+			numBids:      0,
+			listBefore:   true,
+			endedListing: true,
+			expectPass:   false,
+		},
+		{
+			testCase:     "successful cancel without cancel fee",
+			classId:      "class5",
+			nftId:        "nft5",
+			nftOwner:     acc1,
+			canceller:    acc1,
+			cancelAfter:  time.Second * time.Duration(params.NftListingCancelRequiredSeconds+1),
+			numBids:      0,
+			listBefore:   true,
+			endedListing: false,
+			expectPass:   true,
+		},
+		{
+			testCase:     "successful cancel with cancel fee",
+			classId:      "class6",
+			nftId:        "nft6",
+			nftOwner:     acc1,
+			canceller:    acc1,
+			cancelAfter:  time.Second * time.Duration(params.NftListingCancelRequiredSeconds+1),
+			numBids:      0,
+			listBefore:   true,
+			endedListing: false,
+			expectPass:   true,
+		},
+	}
+
+	for _, tc := range tests {
+		suite.app.NFTKeeper.SaveClass(suite.ctx, nfttypes.Class{
+			Id:          tc.classId,
+			Name:        tc.classId,
+			Symbol:      tc.classId,
+			Description: tc.classId,
+			Uri:         tc.classId,
+		})
+		err := suite.app.NFTKeeper.Mint(suite.ctx, nfttypes.NFT{
+			ClassId: tc.classId,
+			Id:      tc.nftId,
+			Uri:     tc.nftId,
+			UriHash: tc.nftId,
+		}, tc.nftOwner)
+		suite.Require().NoError(err)
+
+		nftIdentifier := types.NftIdentifier{ClassId: tc.classId, NftId: tc.nftId}
+		if tc.listBefore {
+			err := suite.app.NftmarketKeeper.ListNft(suite.ctx, &types.MsgListNft{
+				Sender:        ununifitypes.StringAccAddress(tc.nftOwner),
+				NftId:         nftIdentifier,
+				ListingType:   types.ListingType_DIRECT_ASSET_BORROW,
+				BidToken:      "uguu",
+				MinBid:        sdk.ZeroInt(),
+				BidActiveRank: 2,
+			})
+			suite.Require().NoError(err)
+		}
+
+		for i := 0; i < tc.numBids; i++ {
+			bidder := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+
+			// init tokens to addr
+			coin := sdk.NewInt64Coin("uguu", int64(1000000*(i+1)))
+			err = suite.app.BankKeeper.MintCoins(suite.ctx, minttypes.ModuleName, sdk.Coins{coin})
+			suite.NoError(err)
+			err = suite.app.BankKeeper.SendCoinsFromModuleToAccount(suite.ctx, minttypes.ModuleName, bidder, sdk.Coins{coin})
+			suite.NoError(err)
+
+			err := suite.app.NftmarketKeeper.PlaceBid(suite.ctx, &types.MsgPlaceBid{
+				Sender:           ununifitypes.StringAccAddress(bidder),
+				NftId:            nftIdentifier,
+				Amount:           coin,
+				AutomaticPayment: false,
+			})
+			suite.Require().NoError(err)
+		}
+
+		if tc.endedListing {
+			err := suite.app.NftmarketKeeper.EndNftListing(suite.ctx, &types.MsgEndNftListing{
+				Sender: ununifitypes.StringAccAddress(tc.nftOwner),
+				NftId:  nftIdentifier,
+			})
+			suite.Require().NoError(err)
+		}
+
+		oldCancellerBalance := suite.app.BankKeeper.GetBalance(suite.ctx, tc.canceller, "uguu")
+		suite.ctx = suite.ctx.WithBlockTime(suite.ctx.BlockTime().Add(tc.cancelAfter))
+		err = suite.app.NftmarketKeeper.CancelNftListing(suite.ctx, &types.MsgCancelNftListing{
+			Sender: ununifitypes.StringAccAddress(tc.canceller),
+			NftId:  nftIdentifier,
+		})
+
+		if tc.expectPass {
+			suite.Require().NoError(err)
+
+			// check all bids are closed and returned
+			nftBids := suite.app.NftmarketKeeper.GetBidsByNft(suite.ctx, nftIdentifier.IdBytes())
+			suite.Require().Len(nftBids, 0)
+
+			// check cancel fee is reduced from listing owner
+			if tc.numBids > 0 {
+				cancellerNewBalance := suite.app.BankKeeper.GetBalance(suite.ctx, tc.canceller, "uguu")
+				suite.Require().True(cancellerNewBalance.Amount.LT(oldCancellerBalance.Amount))
+			}
+
+			// check nft ownership is returned back to owner
+			owner := suite.app.NFTKeeper.GetOwner(suite.ctx, tc.classId, tc.nftId)
+			suite.Require().Equal(owner, tc.nftOwner)
+
+			// check nft listing is deleted
+			_, err = suite.app.NftmarketKeeper.GetNftListingByIdBytes(suite.ctx, nftIdentifier.IdBytes())
+			suite.Require().Error(err)
+		} else {
+			suite.Require().Error(err)
+		}
+	}
+}
+
 // TODO:Add test for ExpandListingPeriod(ctx sdk.Context, msg *types.MsgExpandListingPeriod) error
 // TODO:Add test for SellingDecision(ctx sdk.Context, msg *types.MsgSellingDecision) error
 // TODO:Add test for EndNftListing(ctx sdk.Context, msg *types.MsgEndNftListing) error
