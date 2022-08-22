053de9b6726f954fd59a68eec80b131a415c1d46
taiki1frsy <taiki.furusyo@gmail.com>
2022-08-22 17:08:59 +0900

    add script to check changes

---
+++ scripts/upgrade-v1-test/3_check_before.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Validators"  >> changed_data.txt
+echo "ununifi1az7xg9wyfruf94teg9u2dr8dec7sjf6esultlc" >> changed_data.txt
+ununifid q bank balances ununifi1az7xg9wyfruf94teg9u2dr8dec7sjf6esultlc >> changed_data.txt
+echo  "ununifi199j70q49338yz9r83xsmfescn74gxachwfp9cs" >> changed_data.txt
+ununifid q bank balances ununifi199j70q49338yz9r83xsmfescn74gxachwfp9cs >> changed_data.txt
+echo "ununifi1w92q2r53jgvyvcuuwmlggrvum7c6j72y403puk" >> changed_data.txt
+ununifid q bank balances ununifi1w92q2r53jgvyvcuuwmlggrvum7c6j72y403puk >> changed_data.txt
+echo "ununifi19f0w9svr905fhefusyx4z8sf83j6et0gkfnrpz" >> changed_data.txt
+ununifid q bank balances ununifi19f0w9svr905fhefusyx4z8sf83j6et0gkfnrpz >> changed_data.txt
+echo " " >> changed_data.txt
+
+echo "Others" >> changed_data.txt 
+echo "ununifi1wqpdt48849gwr6qfslnu0vvrp7k40662w8nt3f" >> changed_data.txt
+ununifid q bank balances ununifi1wqpdt48849gwr6qfslnu0vvrp7k40662w8nt3f >> changed_data.txt
+echo "ununifi1cgjhlgdnzlxp94j9msdl6gxf6qd8eh7wmmdqcn" >> changed_data.txt
+ununifid q bank balances ununifi1cgjhlgdnzlxp94j9msdl6gxf6qd8eh7wmmdqcn >> changed_data.txt
+echo "ununifi1vgjxz84exe55pmvmhrjgd50kuf8nteam9ql3lv" >> changed_data.txt
+ununifid q bank balances ununifi1vgjxz84exe55pmvmhrjgd50kuf8nteam9ql3lv >> changed_data.txt
+echo "ununifi1cxthcxhsks5hfdd5zfmz2t6gkjapvunte4en4q" >> changed_data.txt
+ununifid q bank balances ununifi1cxthcxhsks5hfdd5zfmz2t6gkjapvunte4en4q >> changed_data.txt
+echo "ununifi16m5j7e232uuwgf0qa5ujc9g8rfels40e7jkhxr" >> changed_data.txt
+ununifid q bank balances ununifi16m5j7e232uuwgf0qa5ujc9g8rfels40e7jkhxr >> changed_data.txt
+echo "ununifi1zjee39sw23unvvnw0vxl3fmz33k3s3mfp4ea92" >> changed_data.txt
+ununifid q bank balances ununifi1zjee39sw23unvvnw0vxl3fmz33k3s3mfp4ea92 >> changed_data.txt
+echo "ununifi1vkecuxvetuv86tgnjfkalmtcgy29l37hl74yuk" >> changed_data.txt
+ununifid q bank balances ununifi1vkecuxvetuv86tgnjfkalmtcgy29l37hl74yuk >> changed_data.txt
+echo "ununifi1atmdkat7mqh35u5cpr29njs0qfv33ewn2hdrrv" >> changed_data.txt
+ununifid q bank balances ununifi1atmdkat7mqh35u5cpr29njs0qfv33ewn2hdrrv >> changed_data.txt
+echo "ununifi19570z7f7r2x69056hc5ez3tk9gtau3vyg2zlt7" >> changed_data.txt
+ununifid q bank balances ununifi19570z7f7r2x69056hc5ez3tk9gtau3vyg2zlt7 >> changed_data.txt
+echo " " >> changed_data.txt
+
+echo "Senders" >> changed_data.txt 
+echo "ununifi19srj7ga7t2pyflz7f50le5fv0wa9kuf7tmdtla" >> changed_data.txt
+ununifid q bank balances ununifi19srj7ga7t2pyflz7f50le5fv0wa9kuf7tmdtla >> changed_data.txt
+echo "ununifi1r500cehqg5u6fhsaysmhu4cnw5pz3lxcqhgaq7" >> changed_data.txt
+ununifid q bank balances ununifi1r500cehqg5u6fhsaysmhu4cnw5pz3lxcqhgaq7 >> changed_data.txt
