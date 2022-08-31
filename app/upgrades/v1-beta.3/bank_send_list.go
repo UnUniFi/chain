7970553c55558aa8f55475478ba28763e456a64a
taiki1frsy <taiki.furusyo@gmail.com>
2022-08-31 12:27:49 +0900

    fix the amounts in validator sec

--- app/upgrades/v1-beta.3/bank_send_list.go
+++ app/upgrades/v1-beta.3/bank_send_list.go
@@ -22,14 +22,14 @@ const BANK_SEND_LIST string = `{
 	"validator": [
     {
       "toAddress": "ununifi1az7xg9wyfruf94teg9u2dr8dec7sjf6esultlc",
-      "amount": 2794117647058,
+      "amount": 2328411000000,
       "denom": "uguu",
       "vesting_starts": 1661990400,
       "vesting_ends": 1725148800
     },
     {
       "toAddress": "ununifi199j70q49338yz9r83xsmfescn74gxachwfp9cs",
-      "amount": 2794117647058,
+      "amount": 2328411000000,
       "denom": "uguu",
 
       "vesting_starts": 1661990400,
