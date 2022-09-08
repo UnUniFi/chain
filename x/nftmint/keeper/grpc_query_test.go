d52cb7ce7d32ea4db67e790c943d527ea0e57a83
taiki1frsy <taiki.furusyo@gmail.com>
2022-09-08 15:18:39 +0900

    add success case tests for query of classIdsByOwner and classIdsByName

--- x/nftmint/keeper/grpc_query_test.go
+++ x/nftmint/keeper/grpc_query_test.go
@@ -44,3 +44,45 @@ func (suite *KeeperTestSuite) TestQueryNftMinter() {
 	suite.Require().NoError(err)
 	suite.Require().Equal(sender.String(), res.Minter)
 }
+
+func (suite *KeeperTestSuite) TestQueryClassIdsByOwner() {
+	sender := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	sender_seq, _ := suite.app.AccountKeeper.GetSequence(suite.ctx, sender)
+	classId := keeper.CreateClassId(sender_seq, sender)
+	_ = suite.CreateClass(suite.ctx, classId, sender)
+	_ = suite.MintNFT(suite.ctx, classId, testNFTId, sender)
+
+	req := types.QueryClassIdsByOwnerRequest{
+		Owner: sender.String(),
+	}
+	res, err := suite.app.NftmintKeeper.ClassIdsByOwner(suite.ctx, &req)
+	suite.Require().NoError(err)
+	var classIds []string
+	classIds = append(classIds, classId)
+	expectedRes := types.OwningClassIdList{
+		Owner:   sender.Bytes(),
+		ClassId: classIds[:],
+	}
+	suite.Require().Equal(&expectedRes, res.OwningClassIdList)
+}
+
+func (suite *KeeperTestSuite) TestQueryIdsByName() {
+	sender := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
+	sender_seq, _ := suite.app.AccountKeeper.GetSequence(suite.ctx, sender)
+	classId := keeper.CreateClassId(sender_seq, sender)
+	_ = suite.CreateClass(suite.ctx, classId, sender)
+	_ = suite.MintNFT(suite.ctx, classId, testNFTId, sender)
+
+	req := types.QueryClassIdsByNameRequest{
+		ClassName: testName,
+	}
+	res, err := suite.app.NftmintKeeper.ClassIdsByName(suite.ctx, &req)
+	suite.Require().NoError(err)
+	var classIds []string
+	classIds = append(classIds, classId)
+	expectedRes := types.ClassNameIdList{
+		ClassName: testName,
+		ClassId:   classIds,
+	}
+	suite.Require().Equal(&expectedRes, res.ClassNameIdList)
+}
