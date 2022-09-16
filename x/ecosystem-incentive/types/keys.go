5585c1419e2bb45713bba5997e4f54c0d77d3f54
taiki1frsy <taiki.furusyo@gmail.com>
2022-09-16 17:48:53 +0900

    initial proto type definition

--- x/ecosystem-incentive/types/keys.go
+++ x/ecosystem-incentive/types/keys.go
@@ -1,1 +1,15 @@
 package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "ecosystemincentive"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
+
+	// MemStoreKey defines the in-memory store key
+	MemStoreKey = "mem_ecosystemincentive"
+)
