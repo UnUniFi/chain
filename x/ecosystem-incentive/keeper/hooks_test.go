7d35219364782d70377032e704f68ebf309d2b94
taiki1frsy <taiki.furusyo@gmail.com>
2022-11-30 12:21:44 +0900

    add test for AfterNftPaymentWithCommission

--- x/ecosystem-incentive/keeper/hooks_test.go
+++ x/ecosystem-incentive/keeper/hooks_test.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	ununifitypes "github.com/UnUniFi/chain/types"
+	"github.com/UnUniFi/chain/x/ecosystem-incentive/keeper"
 	"github.com/UnUniFi/chain/x/ecosystem-incentive/types"
 	nftmarkettypes "github.com/UnUniFi/chain/x/nftmarket/types"
 )
@@ -132,7 +133,112 @@ func (suite *KeeperTestSuite) TestAfterNftListed() {
 	}
 }
 
-// TODO: test
-// func (suite *KeeperTestSuite) TestAfterNftPaymentWithCommission()
+func (suite *KeeperTestSuite) TestAfterNftPaymentWithCommission() {
+	tests := []struct {
+		testCase        string
+		nftId           nftmarkettypes.NftIdentifier
+		reward          sdk.Coin
+		incentiveUnitId string
+		subjectAddrs    []ununifitypes.StringAccAddress
+		weights         []sdk.Dec
+		recordedBefore  bool
+		expectPass      bool
+	}{
+		{
+			testCase: "failure case since incentive unit id was not recorded with nft id",
+			nftId: nftmarkettypes.NftIdentifier{
+				ClassId: "class1",
+				NftId:   "nft1",
+			},
+			reward:          sdk.Coin{"uguu", sdk.NewInt(100)},
+			incentiveUnitId: "incentiveUnitId1",
+			subjectAddrs: []ununifitypes.StringAccAddress{
+				ununifitypes.StringAccAddress(suite.addrs[0]),
+			},
+			weights:        []sdk.Dec{sdk.MustNewDecFromStr("1")},
+			recordedBefore: false,
+			expectPass:     false,
+		},
+		{
+			testCase: "failure case since there's no fee to distribute",
+			nftId: nftmarkettypes.NftIdentifier{
+				ClassId: "class2",
+				NftId:   "nft2",
+			},
+			reward:          sdk.Coin{"uguu", sdk.NewInt(0)},
+			incentiveUnitId: "incentiveUnitId1",
+			subjectAddrs: []ununifitypes.StringAccAddress{
+				ununifitypes.StringAccAddress(suite.addrs[0]),
+			},
+			weights:        []sdk.Dec{sdk.MustNewDecFromStr("1")},
+			recordedBefore: true,
+			expectPass:     false,
+		},
+		{
+			testCase: "ordinal case",
+			nftId: nftmarkettypes.NftIdentifier{
+				ClassId: "class3",
+				NftId:   "nft3",
+			},
+			reward:          sdk.Coin{"uguu", sdk.NewInt(100)},
+			incentiveUnitId: "incentiveUnitId3",
+			subjectAddrs: []ununifitypes.StringAccAddress{
+				ununifitypes.StringAccAddress(suite.addrs[0]),
+			},
+			weights:        []sdk.Dec{sdk.MustNewDecFromStr("1")},
+			recordedBefore: true,
+			expectPass:     true,
+		},
+		{
+			testCase: "ordinal case",
+			nftId: nftmarkettypes.NftIdentifier{
+				ClassId: "class4",
+				NftId:   "nft4",
+			},
+			reward:          sdk.Coin{"uguu", sdk.NewInt(100)},
+			incentiveUnitId: "incentiveUnitId4",
+			subjectAddrs: []ununifitypes.StringAccAddress{
+				ununifitypes.StringAccAddress(suite.addrs[0]),
+			},
+			weights:        []sdk.Dec{sdk.MustNewDecFromStr("1")},
+			recordedBefore: true,
+			expectPass:     true,
+		},
+	}
+
+	for _, tc := range tests {
+		suite.SetupTest()
+		_, err := suite.app.EcosystemincentiveKeeper.Register(suite.ctx, &types.MsgRegister{
+			Sender:          tc.subjectAddrs[0],
+			IncentiveUnitId: tc.incentiveUnitId,
+			SubjectAddrs:    tc.subjectAddrs,
+			Weights:         tc.weights,
+		})
+		suite.Require().NoError(err)
+		if tc.recordedBefore {
+			suite.app.EcosystemincentiveKeeper.RecordIncentiveUnitIdWithNftId(suite.ctx, tc.nftId, tc.incentiveUnitId)
+		}
+
+		suite.app.EcosystemincentiveKeeper.Hooks().AfterNftPaymentWithCommission(suite.ctx, tc.nftId, tc.reward)
+
+		if tc.expectPass {
+			_, rewardsForEach := keeper.CalculateRewardsForEachSubject(tc.weights, tc.reward, suite.app.EcosystemincentiveKeeper.GetNftmarketFrontendRewardRate(suite.ctx))
+			for i, subject := range tc.subjectAddrs {
+				rewardStore, exists := suite.app.EcosystemincentiveKeeper.GetRewardStore(suite.ctx, sdk.AccAddress(subject))
+				suite.Require().True(exists)
+				suite.Require().Equal(sdk.NewCoins(sdk.NewCoins(rewardsForEach[i])...), rewardStore.Rewards)
+			}
+		} else {
+			for _, subject := range tc.subjectAddrs {
+				_, exists := suite.app.EcosystemincentiveKeeper.GetRewardStore(suite.ctx, sdk.AccAddress(subject))
+				suite.Require().False(exists)
+			}
+		}
+
+		_, exists := suite.app.EcosystemincentiveKeeper.GetIncentiveUnitIdByNftId(suite.ctx, tc.nftId)
+		suite.Require().False(exists)
+	}
+}
+
 // TODO: test
 // func (suite *KeeperTestSuite) TestAfterNftUnlistedWithoutPayment()
