ea825a8fad529f47b347d07847da86f163aa5ec0
taiki1frsy <taiki.furusyo@gmail.com>
2022-12-07 17:20:48 +0900

    make a condition to return if memo doesn't exist and remove unused line

--- x/ecosystem-incentive/keeper/hooks.go
+++ x/ecosystem-incentive/keeper/hooks.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/UnUniFi/chain/x/ecosystem-incentive/types"
@@ -21,13 +19,16 @@ func (k Keeper) Hooks() Hooks { return Hooks{k} }
 // ------------------- Nftmarket Module Hooks -------------------
 
 func (h Hooks) AfterNftListed(ctx sdk.Context, nftIdentifier nftmarkettypes.NftIdentifier, txMemo string) {
+	if len(txMemo) == 0 {
+		return
+	}
+
 	memoInputs, err := types.ParseMemo([]byte(txMemo))
 
 	// return immediately after emitting event to tell decoding failed
 	// if memo data cannot be decoded properly
 	// this doesn't mean MsgListNft fail. It succeeds anyway.
 	if err != nil {
-		_ = fmt.Errorf(err.Error())
 		_ = ctx.EventManager().EmitTypedEvent(&types.EventFailedParsingMemoInputs{
 			ClassId: nftIdentifier.ClassId,
 			NftId:   nftIdentifier.NftId,
