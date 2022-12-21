271820421517e3b1d2202180ecb7b0ecbe6090de
hikaruNagamine <59303144+hikaruNagamine@users.noreply.github.com>
2022-12-21 04:22:27 +0000

    fix: use genesis-mainnet-mock.json

--- scripts/upgrade-v1-beta4-test/1_init_ununifid.sh
+++ scripts/upgrade-v1-beta4-test/1_init_ununifid.sh
@@ -8,7 +8,9 @@ set -e
 cd $HOME
 sudo rm -rf ~/.ununifi
 ununifid init ununifi-upgrade-test-v1-beta4 --chain-id ununifi-upgrade-test--beta4
-wget https://raw.githubusercontent.com/UnUniFi/network/test/upgrade-v1/test/upgrade-v1-beta4/genesis-mainnet-mock-added-users-upgrade-v1-beta4.json -O  ~/.ununifi/config/genesis.json
+wget https://raw.githubusercontent.com/UnUniFi/network/test/upgrade-v1/test/genesis-mainnet-mock.json  -O  ~/.ununifi/config/genesis.json;
+sed -i 's/ununifi-upgrade-test-v1/ununifi-upgrade-test-v1-beta4/' ~/.ununifi/config/genesis.json;
+
 sed -i '/\[api\]/,+3 s/enable = false/enable = true/' ~/.ununifi/config/app.toml;
 sed -i 's/minimum-gas-prices = ".*"/minimum-gas-prices = "0uguu"/' ~/.ununifi/config/app.toml;
 sed -i 's/stake/uguu/g' ~/.ununifi/config/genesis.json;
