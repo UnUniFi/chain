396221b97b8a428732b1285106e0828ab1567254
taiki1frsy <taiki.furusyo@gmail.com>
2022-12-22 17:03:38 +0900

    set data in init genesis method

--- x/nftmint/genesis.go
+++ x/nftmint/genesis.go
@@ -10,6 +10,17 @@ import (
 // InitGenesis initializes the store state from a genesis state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, accountKeeper types.AccountKeeper, gs types.GenesisState) {
 	k.SetParamSet(ctx, gs.Params)
+	for _, classAttributes := range gs.ClassAttributesList {
+		k.SetClassAttributes(ctx, *classAttributes)
+	}
+
+	for _, classNameIdList := range gs.ClassNameIdLists {
+		k.SetClassNameIdList(ctx, *classNameIdList)
+	}
+
+	for _, owningClassIdList := range gs.OwningClassIdLists {
+		k.SetOwningClassIdList(ctx, *owningClassIdList)
+	}
 }
 
 // ExportGenesis export genesis state for nftmarket module
