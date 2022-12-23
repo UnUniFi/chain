bfbfa2c679dd883c8ea2f1e1c880d7da4fa0ecd3
taiki1frsy <taiki.furusyo@gmail.com>
2022-12-23 17:41:46 +0900

    impl methods of incentiveUnitIdsByAddr  and insert them in a method

--- x/ecosystem-incentive/keeper/incentive_unit_service.go
+++ x/ecosystem-incentive/keeper/incentive_unit_service.go
@@ -5,6 +5,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
+	ununifitypes "github.com/UnUniFi/chain/types"
 	"github.com/UnUniFi/chain/x/ecosystem-incentive/types"
 )
 
@@ -37,6 +38,18 @@ func (k Keeper) Register(ctx sdk.Context, msg *types.MsgRegister) (*[]types.Subj
 		return nil, err
 	}
 
+	// operation related to IncentiveUnitIdsByAddr
+	// if exists already, add incentuve unit id in msg into data
+	// if not, newly create and set
+	for _, addr := range msg.SubjectAddrs {
+		incentiveUnitIdsByAddr := k.GetIncentiveUnitIdsByAddr(ctx, addr)
+		incentiveUnitIdsByAddr = incentiveUnitIdsByAddr.CreateOrUpdate(addr, msg.IncentiveUnitId)
+
+		if err := k.SetIncentiveUnitIdsByAddr(ctx, incentiveUnitIdsByAddr); err != nil {
+			return nil, err
+		}
+	}
+
 	return &subjectInfoList, nil
 }
 
@@ -49,6 +62,19 @@ func (k Keeper) SetIncentiveUnit(ctx sdk.Context, incentiveUnit types.IncentiveU
 	store := ctx.KVStore(k.storeKey)
 	prefixStore := prefix.NewStore(store, []byte(types.KeyPrefixIncentiveUnit))
 	prefixStore.Set([]byte(incentiveUnit.Id), bz)
+
+	return nil
+}
+
+func (k Keeper) SetIncentiveUnitIdsByAddr(ctx sdk.Context, incentiveUnitIdsByAddr types.IncentiveUnitIdsByAddr) error {
+	bz, err := k.cdc.Marshal(&incentiveUnitIdsByAddr)
+	if err != nil {
+		return err
+	}
+
+	store := ctx.KVStore(k.storeKey)
+	prefixStore := prefix.NewStore(store, []byte(types.KeyPrefixIncentiveUnitIdsByAddr))
+	prefixStore.Set(incentiveUnitIdsByAddr.Address, bz)
 
 	return nil
 }
@@ -67,9 +93,30 @@ func (k Keeper) GetIncentiveUnit(ctx sdk.Context, id string) (types.IncentiveUni
 	return incentiveUnit, true
 }
 
+func (k Keeper) GetIncentiveUnitIdsByAddr(ctx sdk.Context, address ununifitypes.StringAccAddress) types.IncentiveUnitIdsByAddr {
+	store := ctx.KVStore(k.storeKey)
+	prefixStore := prefix.NewStore(store, []byte(types.KeyPrefixIncentiveUnitIdsByAddr))
+
+	bz := prefixStore.Get(address)
+	if len(bz) == 0 {
+		return types.IncentiveUnitIdsByAddr{}
+	}
+
+	var incentiveUnitIdsByAddr types.IncentiveUnitIdsByAddr
+	k.cdc.MustUnmarshal(bz, &incentiveUnitIdsByAddr)
+	return incentiveUnitIdsByAddr
+}
+
 func (k Keeper) DeleteIncentiveUnit(ctx sdk.Context, id string) {
 	store := ctx.KVStore(k.storeKey)
 	prefixStore := prefix.NewStore(store, []byte(types.KeyPrefixIncentiveUnit))
 
 	prefixStore.Delete([]byte(id))
+}
+
+func (k Keeper) DeleteIncentiveUnitIdsByAddr(ctx sdk.Context, address ununifitypes.StringAccAddress) {
+	store := ctx.KVStore(k.storeKey)
+	prefixStore := prefix.NewStore(store, []byte(types.KeyPrefixIncentiveUnitIdsByAddr))
+
+	prefixStore.Delete(address)
 }
