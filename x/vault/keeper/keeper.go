895b966848dc559b9d2e3d5b91def89c48a26be1
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-01-13 18:40:41 +0900

    add: function

--- x/vault/keeper/keeper.go
+++ x/vault/keeper/keeper.go
@@ -57,28 +57,78 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 // func (k Keeper) SendCoins(ctx sdk.Context, key []byte, fromAddr sdk.AccAddress, toAddr sdk.AccAddress, amt sdk.Coins) error {
 // 	return nil
 // }
+
 func (k Keeper) SendCoinsFromModuleToAccount(ctx sdk.Context, key []byte, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error {
+	balances := k.GetVaultAmount(ctx, key)
+	afterBalances := k.subAmount(balances, amt)
+	if !afterBalances.IsValid() {
+		// To Do use crisis
+		fmt.Println(afterBalances)
+	}
+	return k.bankKeeper.SendCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, amt)
+}
+
+func (k Keeper) SendCoinsFromModuleToModule(ctx sdk.Context, key []byte, senderModule, recipientModule string, amt sdk.Coins) error {
+	balances := k.GetVaultAmount(ctx, key)
+	afterBalances := k.subAmount(balances, amt)
+	if !afterBalances.IsValid() {
+		// To Do use crisis
+		fmt.Println(afterBalances)
+	}
+	return k.bankKeeper.SendCoinsFromModuleToModule(ctx, senderModule, recipientModule, amt)
+}
+
+func (k Keeper) SendCoinsFromAccountToModule(ctx sdk.Context, key []byte, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
+	newAmount := k.CheckVaultAndAddAmount(ctx, key, amt)
+	k.SetVaultAmount(ctx, key, newAmount)
+	return k.bankKeeper.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, amt)
+}
+
+// function for controlling sdk.Coins
+func (k Keeper) CheckVaultAndAddAmount(ctx sdk.Context, key []byte, amt sdk.Coins) sdk.Coins {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(key)
-	var p sdk.Coins
-	err := json.Unmarshal(bz, &p)
+	if bz != nil {
+		var p sdk.Coins
+		err := json.Unmarshal(bz, &p)
+		if err != nil {
+			panic(err)
+		}
+		k.addAmount(p, amt)
+	}
+	return amt
+}
+
+func (k Keeper) addAmount(balance sdk.Coins, amt sdk.Coins) sdk.Coins {
+	for _, coin := range amt {
+		balance = balance.Add(coin)
+	}
+	return balance
+}
+
+func (k Keeper) subAmount(balance sdk.Coins, amt sdk.Coins) sdk.Coins {
+	for _, coin := range amt {
+		balance = balance.Sub(coin)
+	}
+	return balance
+}
+
+func (k Keeper) GetVaultAmount(ctx sdk.Context, key []byte) sdk.Coins {
+	store := ctx.KVStore(k.storeKey)
+	bz := store.Get(key)
+	var amt sdk.Coins
+	err := json.Unmarshal(bz, &amt)
 	if err != nil {
 		panic(err)
 	}
-	// calc
-	// k.bankKeeper
-	return nil
+	return amt
 }
-func (k Keeper) SendCoinsFromModuleToModule(ctx sdk.Context, key []byte, senderModule, recipientModule string, amt sdk.Coins) error {
-	return nil
-}
-func (k Keeper) SendCoinsFromAccountToModule(ctx sdk.Context, key []byte, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
+
+func (k Keeper) SetVaultAmount(ctx sdk.Context, key []byte, amt sdk.Coins) {
 	store := ctx.KVStore(k.storeKey)
 	bz, err := amt.MarshalJSON()
 	if err != nil {
 		panic(err)
 	}
 	store.Set(key, bz)
-	// k.bankKeeper
-	return nil
 }
