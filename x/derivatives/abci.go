76eb47fc0bd5538e9f9c4d5a777485a7f238bcd7
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-01-25 11:03:36 +0800

    chore: comments

--- x/derivatives/abci.go
+++ x/derivatives/abci.go
@@ -6,13 +6,22 @@ import (
 	"github.com/UnUniFi/chain/x/derivatives/keeper"
 )
 
-// BeginBlocker
-func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
-	// TODO: impose imaginary funding rate
-	// TODO: cutting losses
+func levyImaginaryFundingRateAndLiquidateInsufficientMarginPositions(ctx sdk.Context, k keeper.Keeper) {
+	// TODO: Iterate open position by for loop and levy imaginary funding rate from margin (principal, collateral).
+	// imaginary_funding_rate increases in proportion to the net position of traders.
+	// If traders' net position is long, opened long positions are levied the imaginary funding rate and opened short positions get the funding rate (without imaginary funding rate commission rate).
+	// If traders' net position is short, opened short positions are levied the imaginary funding rate and opened long positions get the funding rate (without imaginary funding rate commission rate).
+	// imaginary funding rate commission rate is needed to be defined in Params
+	// TODO: If position_profit - principal is close to zero (e.g. under 50% of principal), forcibly close the position.
+	// This "50%" should also be parameterized in Params. The name should be "margin_maintenance_rate": sdk.Dec
+}
+
+func setPoolMarketCapSnapshot(ctx sdk.Context, k keeper.Keeper) {
+	k.SetPoolMarketCapSnapshot(ctx, ctx.BlockHeight(), k.GetPoolMarketCap(ctx))
 }
 
 // EndBlocker
 func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
-	k.SetPoolMarketCapSnapshot(ctx, ctx.BlockHeight(), k.GetPoolMarketCap(ctx))
+	levyImaginaryFundingRateAndLiquidateInsufficientMarginPositions(ctx, k)
+	setPoolMarketCapSnapshot(ctx, k)
 }
