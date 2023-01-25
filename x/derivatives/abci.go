69bf98a4cf7c4ebdd01e9684ff694267a846a7cd
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-01-25 11:06:52 +0800

    chore: comments

--- x/derivatives/abci.go
+++ x/derivatives/abci.go
@@ -9,8 +9,9 @@ import (
 func levyImaginaryFundingRateAndLiquidateInsufficientMarginPositions(ctx sdk.Context, k keeper.Keeper) {
 	// TODO: Iterate open position by for loop and levy imaginary funding rate from margin (principal, collateral).
 	// imaginary_funding_rate increases in proportion to the net position of traders.
-	// If traders' net position is long, opened long positions are levied the imaginary funding rate and opened short positions get the funding rate (without imaginary funding rate commission rate).
-	// If traders' net position is short, opened short positions are levied the imaginary funding rate and opened long positions get the funding rate (without imaginary funding rate commission rate).
+	// If traders' net position is long, imaginary funding rate is positive. If traders' net position is short, imaginary funding rate is negative.
+	// If FR is positive, opened long positions are levied the imaginary funding rate and opened short positions get the funding rate (without imaginary funding rate commission rate).
+	// If FR is negative, opened short positions are levied the imaginary funding rate and opened long positions get the funding rate (without imaginary funding rate commission rate).
 	// imaginary funding rate commission rate is needed to be defined in Params
 	// TODO: If position_profit - principal is close to zero (e.g. under 50% of principal), forcibly close the position.
 	// This "50%" should also be parameterized in Params. The name should be "margin_maintenance_rate": sdk.Dec
