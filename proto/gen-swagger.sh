311600025ac73d5816ea4e99565a45e04c6248f2
hikaruNagamine <59303144+hikaruNagamine@users.noreply.github.com>
2023-02-06 23:28:24 +0900

    fix: generate swagger. buf protoc -> buf generate

--- proto/gen-swagger.sh
+++ proto/gen-swagger.sh
@@ -4,20 +4,9 @@
 set -eo pipefail
 
 mkdir -p ./tmp-swagger-gen
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-for dir in $proto_dirs; do
 
-  # generate swagger files (filter query files)
-  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
-      -I "proto" \
-      -I "proto-thirdparty" \
-      "$query_file" \
-      --swagger_out=./tmp-swagger-gen \
-      --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
-  fi
-done
+# generate swagger files (filter query files)
+buf generate --template ./proto/buf.gen.swagger.yaml
 
 # combine swagger files
 # uses nodejs package `swagger-combine`.
