326c49a99cd7efe14e6e07a580ff194b79410b3d
mkXultra <electrincgoat@gmail.com>
2023-02-10 21:09:21 +0900

    modify: remove unessassry comment

--- x/derivatives/genesis.go
+++ x/derivatives/genesis.go
@@ -11,12 +11,10 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
-	// loop markets
 	for _, market := range genState.Params.PerpetualFutures.Markets {
 		// set initial net position
 		k.SetPerpetualFuturesNetPositionOfMarket(ctx, *market, sdk.NewDec(0))
 	}
-	// k.SetPerpetualFuturesNetPositionOfMarket(ctx, market, sdk.NewDec(0))
 }
 
 // ExportGenesis returns the module's exported genesis
