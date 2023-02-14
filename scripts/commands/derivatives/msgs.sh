7f7676bf01d7c883440351c511e1ab8bb0a7dc66
taiki1frsh <taiki.furusho@gmail.com>
2023-02-14 09:50:13 +0900

    make open-position work

--- scripts/commands/derivatives/msgs.sh
+++ scripts/commands/derivatives/msgs.sh
@@ -9,4 +9,4 @@ ununifid tx derivatives mint-lpt 100000ubtc --from=debug --chain-id=test --broad
 
 # error show command
 # ununifid tx derivatives open-position perpetual-futures 100ubtc 10ubtc 10usd  --from=$USER1 $conf | jq .raw_log | sed 's/\\n/\n/g'
-ununifid tx derivatives open-position perpetual-futures 100ubtc 10ubtc 10usd  --from=$USER1 $conf | jq .
+ununifid tx derivatives open-position perpetual-futures 100ubtc ubtc uusd  --from=$USER1 $conf | jq .
