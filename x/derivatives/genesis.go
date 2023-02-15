49d440347ccff0fa1241ec8972b615c33e386653
mkXultra <electrincgoat@gmail.com>
2023-02-15 10:40:43 +0900

    modify: load gen when start app

--- x/derivatives/genesis.go
+++ x/derivatives/genesis.go
@@ -11,6 +11,14 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
+	// todo load genesis state when restart
+	for _, asset := range genState.Params.Pool.AcceptedAssets {
+		k.AddPoolAsset(ctx, *asset)
+	}
+	for _, market := range genState.Params.PerpetualFutures.Markets {
+		// set initial net position
+		k.SetPerpetualFuturesNetPositionOfMarket(ctx, *market, sdk.NewDec(0))
+	}
 }
 
 // ExportGenesis returns the module's exported genesis
