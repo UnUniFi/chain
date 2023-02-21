4d3a374bd11afd0c608e67c0ed0481dc1e8fd7cd
mkXultra <electrincgoat@gmail.com>
2023-02-21 18:00:51 +0900

    feat: run msg report in begin block

--- x/derivatives/abci.go
+++ x/derivatives/abci.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/UnUniFi/chain/x/derivatives/keeper"
+	"github.com/UnUniFi/chain/x/derivatives/types"
 )
 
 func setPoolMarketCapSnapshot(ctx sdk.Context, k keeper.Keeper) {
@@ -24,6 +25,29 @@ func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
 	// TODO: make this function calling every 8 hours.
 	// saving `last_levy_ifr_block_time` in store is one of ways to do so.
 	// levyImaginaryFundingRate(ctx, k)
+	CheckPotision(ctx, k)
+}
+
+// todo: fixme this function is temporary treatment.
+func CheckPotision(ctx sdk.Context, k keeper.Keeper) {
+	potisions := k.GetAllPositions(ctx)
+	params := k.GetParams(ctx)
+	for _, potision := range potisions {
+		if potision.NeedLiqudation(params.PerpetualFutures.MarginMaintenanceRate) {
+			msg := types.MsgReportLiquidation{
+				Sender:          potision.Address,
+				PositionId:      potision.Id,
+				RewardRecipient: potision.Address,
+			}
+			k.ReportLiquidation(ctx, &msg)
+		}
+		msg := types.MsgReportLevyPeriod{
+			Sender:          potision.Address,
+			PositionId:      potision.Id,
+			RewardRecipient: potision.Address,
+		}
+		k.ReportLevyPeriod(ctx, &msg)
+	}
 }
 
 // EndBlocker
--- x/derivatives/types/positions.go
+++ x/derivatives/types/positions.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/gogo/protobuf/proto"
 )
 
@@ -23,4 +24,33 @@ func UnpackPositionInstance(positionAny types.Any) (PositionInstance, error) {
 	}
 
 	return nil, fmt.Errorf("this Any doesn't have PositionInstance value")
+}
+
+func (m Position) NeedLiqudation(MarginMaintenanceRate sdk.Dec) bool {
+	ins, err := UnpackPositionInstance(m.PositionInstance)
+	if err != nil {
+		return false
+	}
+
+	switch positionInstance := ins.(type) {
+	case *PerpetualFuturesPositionInstance:
+		return m.NeedLiqudationPerpetualFutures(MarginMaintenanceRate, *positionInstance)
+		break
+	case *PerpetualOptionsPositionInstance:
+		panic("not implemented")
+		break
+	default:
+		panic("not implemented")
+	}
+	return false
+}
+
+func (m Position) NeedLiqudationPerpetualFutures(MarginMaintenanceRate sdk.Dec, positionInstance PerpetualFuturesPositionInstance) bool {
+	marginDec := sdk.NewDecFromInt(m.RemainingMargin.Amount).Mul(sdk.NewDecWithPrec(1, 0))
+	principal := positionInstance.CalculatePrincipal().Mul(MarginMaintenanceRate)
+	if marginDec.LT(principal) {
+		return true
+	} else {
+		return false
+	}
 }
