65fa29508b597d5344c6cc43cf6962d2d6c386e9
jununifi <jun.ununifi@outlook.com>
2023-02-28 20:42:44 +0800

    add TestBlockTimestampGetSet

--- x/derivatives/keeper/yield_test.go
+++ x/derivatives/keeper/yield_test.go
@@ -24,10 +24,22 @@ func (suite *KeeperTestSuite) TestAnnualizeYieldRate() {
 	suite.Require().Equal(annualizedYieldRate, sdk.NewDec(730))
 }
 
+func (suite *KeeperTestSuite) TestBlockTimestampGetSet() {
+	unsavedTime := suite.keeper.GetBlockTimestamp(suite.ctx, 1)
+	suite.Require().Equal(unsavedTime, time.Time{})
+
+	now := time.Now()
+	future := time.Now().Add(time.Second * 43200)
+	suite.keeper.SaveBlockTimestamp(suite.ctx, 1, now)
+	savedTime := suite.keeper.GetBlockTimestamp(suite.ctx, 1)
+	suite.Require().Equal(savedTime.Unix(), now.Unix())
+
+	suite.keeper.SaveBlockTimestamp(suite.ctx, 1, future)
+	savedTime = suite.keeper.GetBlockTimestamp(suite.ctx, 1)
+	suite.Require().Equal(savedTime.Unix(), future.Unix())
+}
+
 // TODO: add test for
-// func (k Keeper) SaveBlockTimestamp(ctx sdk.Context, height int64, blockTime time.Time) {
-// func (k Keeper) GetBlockTimestamp(ctx sdk.Context, height int64) time.Time {
 // func (k Keeper) GetLPNominalYieldRate(ctx sdk.Context, beforeHeight int64, afterHeight int64) sdk.Dec {
 // func (k Keeper) GetInflationRateOfAssetsInPool(ctx sdk.Context, beforeHeight int64, afterHeight int64) sdk.Dec {
 // func (k Keeper) GetLPRealYieldRate(ctx sdk.Context, beforeHeight int64, afterHeight int64) sdk.Dec {
-// func (k Keeper) AnnualizeYieldRate(ctx sdk.Context, yieldRate sdk.Dec, beforeHeight int64, afterHeight int64) sdk.Dec {
