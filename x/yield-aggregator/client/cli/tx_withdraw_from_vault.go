e841f970ca826ab6be40f623db020676ebad2a66
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-03-09 17:35:12 +0800

    feat: tx cli

--- x/yield-aggregator/client/cli/tx_withdraw_from_vault.go
+++ x/yield-aggregator/client/cli/tx_withdraw_from_vault.go
@@ -1,1 +1,44 @@
 package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+
+	"github.com/UnUniFi/chain/x/yield-aggregator/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func CmdTxWithdrawFromVault() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "withdraw-from-vault [id] [lp-token-amount]",
+		Short: "withdraw from the vault",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			id, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			lpAmount, err := sdk.ParseCoinNormalized(args[1])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgWithdrawFromVault(clientCtx.GetFromAddress().String(), id, lpAmount.Amount)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
---
+++ x/yield-aggregator/types/message_transfer_vault_ownership.go
@@ -0,0 +1,47 @@
+package types
+
+import (
+	// errorsmod "cosmossdk.io/errors"
+	// "github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+const TypeMsgTransferVaultOwnership = "transfer-vault-ownership"
+
+var _ sdk.Msg = &MsgCreateVault{}
+
+func NewMsgTransferVaultOwnership(sender string, vaultId uint64, recipient string) *MsgTransferVaultOwnership {
+	return &MsgTransferVaultOwnership{
+		Sender:    sender,
+		VaultId:   vaultId,
+		Recipient: recipient,
+	}
+}
+
+func (msg MsgTransferVaultOwnership) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.Sender); err != nil {
+		return sdkerrors.ErrInvalidAddress.Wrapf("invalid sender address: %s", err)
+	}
+
+	// TODO
+
+	return nil
+}
+
+func (msg *MsgTransferVaultOwnership) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgTransferVaultOwnership) Type() string {
+	return TypeMsgDepositToVault
+}
+
+func (msg MsgTransferVaultOwnership) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(&msg))
+}
+
+func (msg MsgTransferVaultOwnership) GetSigners() []sdk.AccAddress {
+	addr, _ := sdk.AccAddressFromBech32(msg.Sender)
+	return []sdk.AccAddress{addr}
+}
