d8259276cba9c3f498d64c7a5e8c8a20c3a0930a
taiki1frsh <taiki.furusho@gmail.com>
2023-04-03 22:28:30 +0900

    remove unused

--- x/derivatives/types/perpetual_futures.go
+++ x/derivatives/types/perpetual_futures.go
@@ -21,12 +21,6 @@ func UnpackPerpetualFuturesPositionInstance(positionAny types.Any) PositionInsta
 	return nil
 }
 
-// CalculatePrincipal is not used in the entire codebase.
-// TODO: So, maybe we should remove it?
-func (positionInstance PerpetualFuturesPositionInstance) CalculatePrincipal() sdk.Dec {
-	return positionInstance.Size_.Quo(sdk.NewDec(int64(positionInstance.Leverage)))
-}
-
 // Position Size is considered on a micro level in the backend
 func NewPerpetualFuturesNetPositionOfMarket(market Market, position_size_in_micro sdk.Int) PerpetualFuturesNetPositionOfMarket {
 	return PerpetualFuturesNetPositionOfMarket{
