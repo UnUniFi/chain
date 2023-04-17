b3e4f532dc48ad349c99b288228c7598cc1c3b0b
taiki1frsh <taiki.furusho@gmail.com>
2023-04-17 13:32:17 +0900

    fix TestGetAssetTargetAmount

--- x/derivatives/keeper/pool_test.go
+++ x/derivatives/keeper/pool_test.go
@@ -8,7 +8,6 @@ import (
 	pricefeedtypes "github.com/UnUniFi/chain/x/pricefeed/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 )
 
 func (suite *KeeperTestSuite) TestGetAssetBalanceInPoolByDenom() {
@@ -76,31 +75,21 @@ func (suite *KeeperTestSuite) TestIsAssetValid() {
 	suite.Require().False(isValid)
 }
 
+// TODO: implement additional test cases
 func (suite *KeeperTestSuite) TestGetAssetTargetAmount() {
 	// get target amount at initial
 	targetAmount, err := suite.keeper.GetAssetTargetAmount(suite.ctx, "uatom")
 	suite.Require().NoError(err)
 	suite.Require().Equal(targetAmount.String(), "0uatom")
 
-	// set price for asset
-	_, err = suite.app.PricefeedKeeper.SetPrice(suite.ctx, sdk.AccAddress{}, "uatom:uusdc", sdk.NewDec(13), suite.ctx.BlockTime().Add(time.Hour*3))
-	suite.Require().NoError(err)
-	params := suite.app.PricefeedKeeper.GetParams(suite.ctx)
-	params.Markets = []pricefeedtypes.Market{
-		{MarketId: "uatom:uusdc", BaseAsset: "uatom", QuoteAsset: "uusdc", Oracles: []ununifitypes.StringAccAddress{}, Active: true},
-	}
-	suite.app.PricefeedKeeper.SetParams(suite.ctx, params)
-	err = suite.app.PricefeedKeeper.SetCurrentPrices(suite.ctx, "uatom:uusdc")
-	suite.Require().NoError(err)
-
 	// set lp token supply
-	err = suite.app.BankKeeper.MintCoins(suite.ctx, minttypes.ModuleName, sdk.Coins{sdk.NewInt64Coin(types.LiquidityProviderTokenDenom, 1000000)})
+	err = suite.app.BankKeeper.MintCoins(suite.ctx, types.ModuleName, sdk.Coins{sdk.NewInt64Coin("uusdc", 1000000)})
 	suite.Require().NoError(err)
 
 	// get target amount after data set
-	targetAmount, err = suite.keeper.GetAssetTargetAmount(suite.ctx, "uatom")
+	targetAmount, err = suite.keeper.GetAssetTargetAmount(suite.ctx, "uusdc")
 	suite.Require().NoError(err)
-	suite.Require().Equal(targetAmount.String(), "1000000uatom")
+	suite.Require().Equal("500000uusdc", targetAmount.String())
 }
 
 func (suite *KeeperTestSuite) TestIsPriceReady() {
