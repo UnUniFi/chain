38cddd505d826fa6d748c201cfc1291af2cd6808
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-06-07 13:55:01 +0900

    fix: go mod tidy err

--- x/pricefeed/keeper/integration_test.go
+++ x/pricefeed/keeper/integration_test.go
@@ -6,7 +6,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/UnUniFi/chain/app"
-	"github.com/UnUniFi/chain/types"
 	pricefeedtypes "github.com/UnUniFi/chain/x/pricefeed/types"
 )
 
@@ -14,20 +13,20 @@ func NewPricefeedGenStateMulti(tApp app.TestApp) app.GenesisState {
 	pfGenesis := pricefeedtypes.GenesisState{
 		Params: pricefeedtypes.Params{
 			Markets: []pricefeedtypes.Market{
-				{MarketId: "btc:usd", BaseAsset: "btc", QuoteAsset: "usd", Oracles: types.StringAccAddresses([]sdk.AccAddress{}), Active: true},
-				{MarketId: "xrp:usd", BaseAsset: "xrp", QuoteAsset: "usd", Oracles: types.StringAccAddresses([]sdk.AccAddress{}), Active: true},
+				{MarketId: "btc:usd", BaseAsset: "btc", QuoteAsset: "usd", Oracles: []string{}, Active: true},
+				{MarketId: "xrp:usd", BaseAsset: "xrp", QuoteAsset: "usd", Oracles: []string{}, Active: true},
 			},
 		},
 		PostedPrices: []pricefeedtypes.PostedPrice{
 			{
 				MarketId:      "btc:usd",
-				OracleAddress: types.StringAccAddress{},
+				OracleAddress: "",
 				Price:         sdk.MustNewDecFromStr("8000.00"),
 				Expiry:        time.Now().Add(1 * time.Hour),
 			},
 			{
 				MarketId:      "xrp:usd",
-				OracleAddress: types.StringAccAddress{},
+				OracleAddress: "",
 				Price:         sdk.MustNewDecFromStr("0.25"),
 				Expiry:        time.Now().Add(1 * time.Hour),
 			},
