e0e9b8f5607e3173bce908bfeba994c31a45293c
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-06-12 09:35:57 +0800

    feat: icacontroller migration

--- app/upgrades/v3/constants.go
+++ app/upgrades/v3/constants.go
@@ -12,6 +12,7 @@ import (
 	recordstypes "github.com/UnUniFi/chain/x/yieldaggregator/ibcstaking/records/types"
 	stakeibctypes "github.com/UnUniFi/chain/x/yieldaggregator/ibcstaking/stakeibc/types"
 	yieldaggregatortypes "github.com/UnUniFi/chain/x/yieldaggregator/types"
+	icacontrollertypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/controller/types"
 )
 
 const UpgradeName string = "v3"
@@ -20,7 +21,7 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added:   []string{group.ModuleName, epochstypes.ModuleName, icacallbackstypes.ModuleName, interchainquerytypes.ModuleName, recordstypes.ModuleName, stakeibctypes.ModuleName, yieldaggregatortypes.ModuleName},
+		Added:   []string{icacontrollertypes.SubModuleName, group.ModuleName, epochstypes.ModuleName, icacallbackstypes.ModuleName, interchainquerytypes.ModuleName, recordstypes.ModuleName, stakeibctypes.ModuleName, yieldaggregatortypes.ModuleName},
 		Deleted: []string{},
 	},
 }
