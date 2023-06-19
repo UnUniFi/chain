952be3eb4a478273f7c33d45e0706be0d198e274
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2023-06-19 10:41:28 +0800

    feat: clda

--- x/nftbackedloan/types/clda.go
+++ x/nftbackedloan/types/clda.go
@@ -19,51 +19,69 @@ func SortBidsByDeposit(bids []Bid) []Bid {
 	return bids
 }
 
-func NextSettlementRightBidId(bidsSortedByDeposit []Bid, skip map[BidId]bool, borrowed uint64, forfeited uint64) *BidId {
+func NextSettlementRightBidId(bidsSortedByDeposit []Bid, skip map[BidId]bool, forfeited uint64, needToCollectAmount uint64) BidId {
+	var ret BidId
 	for _, bid := range bidsSortedByDeposit {
 		if skip[bid.Id] {
 			continue
 		}
-		if forfeited+bid.Price > borrowed {
-			return &bid.Id
+		if bid.Price+forfeited < needToCollectAmount {
+			continue
 		}
+		ret = bid.Id
+		break
 	}
 
-	return nil
+	return ret
 }
 
-// Simulate repay amount with given borrow amount
-// Utilizing lowest interest rate first
-// No refinancing
-func SimulateRepayAmount(bidsSortedByInterestRate []Bid, borrowAmount uint64, now time.Time, maxDuration time.Duration) uint64 {
-	return 0
-}
-
-// Simulate borrow amount with given repay amount
-// Utilizing lowest interest rate first
-// No refinancing
-func SimulateBorrowAmount(bidsSortedByInterestRate []Bid, repayAmount uint64, now time.Time, maxDuration time.Duration) uint64 {
-
-	return 0
+func AdjustAnnualRateWithDuration(annualInterestRate uint64, duration time.Duration) uint64 {
+	// TODO
+	return annualInterestRate
 }
 
 // Do not consider refinancing
-func BorrowableAmount(bidsSortedByPrice []Bid, bidsSortedByInterestRate []Bid, now time.Time) uint64 {
-	var sumDeposit uint64
-	var closestExpire time.Time
+func BorrowableAmount(bidsSortedByPrice []Bid, bidsSortedByInterestRate []Bid, duration time.Duration) uint64 {
+	var minimumSettlementAmount uint64
+	var forfeitedDeposit uint64
+
 	for _, bid := range bidsSortedByPrice {
-		sumDeposit += bid.Deposit
-		if closestExpire.IsZero() || closestExpire.After(bid.End) {
-			closestExpire = bid.End
+		if minimumSettlementAmount == 0 || minimumSettlementAmount > bid.Price+forfeitedDeposit {
+			minimumSettlementAmount = bid.Price + forfeitedDeposit
 		}
-		maxDuration := closestExpire.Sub(now)
-
-		simulatedRepayAmount := SimulateRepayAmount(bidsSortedByInterestRate, sumDeposit, now, maxDuration)
-
-		if simulatedRepayAmount > bid.Price {
-			return SimulateBorrowAmount(bidsSortedByInterestRate, sumDeposit-bid.Deposit+bid.Price, now, maxDuration)
-		}
+		forfeitedDeposit += bid.Deposit
 	}
 
-	return sumDeposit
+	var repayAmount uint64
+	var borrowableAmount uint64
+
+	for _, bid := range bidsSortedByInterestRate {
+		repayAmount += (1 + AdjustAnnualRateWithDuration(bid.AnnualInterestRate, duration)) * bid.Deposit
+
+		if repayAmount > minimumSettlementAmount {
+			break
+		}
+		borrowableAmount += bid.Deposit
+	}
+
+	return borrowableAmount
+}
+
+func FilterBidsById(bids []Bid, excludeId BidId) []Bid {
+	var ret []Bid
+	for _, bid := range bids {
+		if bid.Id == excludeId {
+			continue
+		}
+		ret = append(ret, bid)
+	}
+	return ret
+}
+
+func IsBidAbleToCancel(cancellingBidId BidId, bidsSortedByPrice []Bid, bidsSortedByInterestRate []Bid, durationToClosestExpiry time.Duration, borrowedAmount uint64) bool {
+	return BorrowableAmount(
+		FilterBidsById(bidsSortedByPrice, cancellingBidId),
+		FilterBidsById(bidsSortedByInterestRate, cancellingBidId),
+		durationToClosestExpiry,
+	) >= borrowedAmount
 }
