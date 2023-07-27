73c03e8cb48c44251586e1616fc8df2830d8695f
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-07-27 20:26:07 +0900

    feat: levy test

---
+++ x/derivatives/keeper/test/04_pool_levy_period.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+## before run this script, change params scripts/setup/init.sh
+## Line 88 levy_period_required_seconds under block speed
+
+# block speed
+sleep=5
+
+user1_address=ununifi155u042u8wk3al32h3vzxu989jj76k4zcu44v6w
+user2_address=ununifi1v0h8j7x7kfys29kj4uwdudcc9y0nx6twwxahla
+init_ubtc_balance=$(ununifid q bank balances ununifi155u042u8wk3al32h3vzxu989jj76k4zcu44v6w --denom ubtc -o json | jq .amount | tr -d '"')
+init_udlp_balance=$(ununifid q bank balances ununifi155u042u8wk3al32h3vzxu989jj76k4zcu44v6w --denom udlp -o json | jq .amount | tr -d '"')
+init_user2_ubtc_balance=$(ununifid q bank balances $user2_address --denom ubtc -o json | jq .amount | tr -d '"')
+
+echo "------------deposit to pool 1st------------"
+ununifid tx derivatives deposit-to-pool 100000000ubtc \
+--from user1 --keyring-backend test --chain-id test --yes
+
+sleep $sleep
+
+user1_udlp_balance=$(ununifid q bank balances ununifi155u042u8wk3al32h3vzxu989jj76k4zcu44v6w --denom udlp -o json | jq .amount | tr -d '"')
+
+echo "------------open position------------"
+ununifid tx derivatives open-position perpetual-futures 10000000ubtc ubtc uusdc long 40 4 \
+--from user2 --keyring-backend test --chain-id test --yes
+
+sleep $sleep
+
+echo "------------opened position------------"
+ununifid q derivatives positions $user2_address
+
+sleep $sleep
+
+echo "------------levy tx------------"
+ununifid tx derivatives report-levy-period 1 $user1_address \
+--from user1 --keyring-backend test --chain-id test --yes
+
+sleep $sleep
+
+echo "------------levied position------------"
+ununifid q derivatives positions $user2_address
+
+rate=$(ununifid q derivatives delp-token-rate -o json | jq .rates[0].amount | tr -d '"')
+
+if [ "$rate" -gt "1000000" ]; then
+  echo "pass: delp token rate is correct: $rate"
+else
+  echo "error: delp token rate is incorrect:"
+  echo "expected: 0 actual: $rate"
+fi
+
+# echo "------------withdraw from pool------------"
+# ununifid tx derivatives withdraw-from-pool $user1_udlp_balance ubtc \
+# --from user1 --keyring-backend test --chain-id test --yes
+
+# sleep $sleep
