c25cdeb7f4298db4fb0e98b709bb97f00178fb00
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-07 02:45:36 +0900

    fix: query proto

--- x/derivatives/types/query.pb.gw.go
+++ x/derivatives/types/query.pb.gw.go
@@ -555,20 +555,20 @@ func local_request_Query_AddressPositions_0(ctx context.Context, marshaler runti
 
 }
 
-func request_Query_PendingPaymentPositions_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryPendingPaymentPositionsRequest
+func request_Query_AllPendingPaymentPositions_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryAllPendingPaymentPositionsRequest
 	var metadata runtime.ServerMetadata
 
-	msg, err := client.PendingPaymentPositions(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.AllPendingPaymentPositions(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Query_PendingPaymentPositions_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq QueryPendingPaymentPositionsRequest
+func local_request_Query_AllPendingPaymentPositions_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq QueryAllPendingPaymentPositionsRequest
 	var metadata runtime.ServerMetadata
 
-	msg, err := server.PendingPaymentPositions(ctx, &protoReq)
+	msg, err := server.AllPendingPaymentPositions(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -1151,7 +1151,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
-	mux.Handle("GET", pattern_Query_PendingPaymentPositions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_AllPendingPaymentPositions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -1162,7 +1162,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Query_PendingPaymentPositions_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Query_AllPendingPaymentPositions_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -1170,7 +1170,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 
-		forward_Query_PendingPaymentPositions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_AllPendingPaymentPositions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1593,7 +1593,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Query_PendingPaymentPositions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_AllPendingPaymentPositions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -1602,14 +1602,14 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Query_PendingPaymentPositions_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Query_AllPendingPaymentPositions_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Query_PendingPaymentPositions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_AllPendingPaymentPositions_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1761,7 +1761,7 @@ var (
 
 	pattern_Query_AddressPositions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"ununifi", "derivatives", "address", "positions"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_PendingPaymentPositions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"ununifi", "derivatives", "pending-payment-positions"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_AllPendingPaymentPositions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"ununifi", "derivatives", "pending-payment-positions"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_PendingPaymentPosition_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"ununifi", "derivatives", "pending-payment-positions", "position_id"}, "", runtime.AssumeColonVerbOpt(false)))
 
@@ -1801,7 +1801,7 @@ var (
 
 	forward_Query_AddressPositions_0 = runtime.ForwardResponseMessage
 
-	forward_Query_PendingPaymentPositions_0 = runtime.ForwardResponseMessage
+	forward_Query_AllPendingPaymentPositions_0 = runtime.ForwardResponseMessage
 
 	forward_Query_PendingPaymentPosition_0 = runtime.ForwardResponseMessage
 
