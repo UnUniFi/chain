35738833b0b8276a08602669c8740c8f1e1f5c77
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-09 00:11:27 +0900

    delete some cli

--- x/ecosystemincentive/client/cli/query.go
+++ x/ecosystemincentive/client/cli/query.go
@@ -25,39 +25,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	cmd.AddCommand(
 		CmdQueryParams(),
-		CmdQueryRecordedRecipientContainerId(),
 		CmdQueryAllRewards(),
-		CmdQueryRecipientContainer(),
-		CmdQueryRecipientContainerIdsByAddr(),
 	)
-
-	return cmd
-}
-
-func CmdQueryRecordedRecipientContainerId() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "recorded-incentive-unit-id [class-id] [nft-id]",
-		Short: "shows incentive-unit-id recorded with the class and nft ids",
-		Args:  cobra.ExactArgs(2),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
-
-			queryClient := types.NewQueryClient(clientCtx)
-
-			req := &types.QueryRecordedRecipientContainerIdRequest{
-				ClassId: args[0],
-				NftId:   args[1],
-			}
-
-			res, err := queryClient.RecordedRecipientContainerId(context.Background(), req)
-			if err != nil {
-				return err
-			}
-			return clientCtx.PrintProto(res)
-		},
-	}
-
-	flags.AddQueryFlagsToCmd(cmd)
 	return cmd
 }
 
@@ -76,58 +45,6 @@ func CmdQueryAllRewards() *cobra.Command {
 			}
 
 			res, err := queryClient.AllRewards(context.Background(), req)
-			if err != nil {
-				return err
-			}
-			return clientCtx.PrintProto(res)
-		},
-	}
-
-	flags.AddQueryFlagsToCmd(cmd)
-	return cmd
-}
-
-func CmdQueryRecipientContainer() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "incentive-unit [incentive-unit-id]",
-		Short: "shows incentive-unit data",
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
-
-			queryClient := types.NewQueryClient(clientCtx)
-
-			req := &types.QueryRecipientContainerRequest{
-				Id: args[0],
-			}
-
-			res, err := queryClient.RecipientContainer(context.Background(), req)
-			if err != nil {
-				return err
-			}
-			return clientCtx.PrintProto(res)
-		},
-	}
-
-	flags.AddQueryFlagsToCmd(cmd)
-	return cmd
-}
-
-func CmdQueryRecipientContainerIdsByAddr() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "incentive-unit-ids-by-addr [address]",
-		Short: "shows incentive-unit-ids to which the address belongs",
-		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			clientCtx := client.GetClientContextFromCmd(cmd)
-
-			queryClient := types.NewQueryClient(clientCtx)
-
-			req := &types.QueryBelongingRecipientContainerIdsByAddrRequest{
-				Address: args[0],
-			}
-
-			res, err := queryClient.BelongingRecipientContainerIdsByAddr(context.Background(), req)
 			if err != nil {
 				return err
 			}
