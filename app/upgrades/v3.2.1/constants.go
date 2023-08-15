08dc616e53ecb5b365e7e5e0db208e612b11c436
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-15 20:26:57 +0900

    for testnet upgrade

--- app/upgrades/v3.2.1/constants.go
+++ app/upgrades/v3.2.1/constants.go
@@ -3,11 +3,7 @@ package v3_2_1
 import (
 	store "github.com/cosmos/cosmos-sdk/store/types"
 
-	ibchookstypes "github.com/cosmos/ibc-apps/modules/ibc-hooks/v7/types"
-	buildertypes "github.com/skip-mev/pob/x/builder/types"
-
 	"github.com/UnUniFi/chain/app/upgrades"
-	nftfactorytypes "github.com/UnUniFi/chain/x/nftfactory/types"
 )
 
 const UpgradeName string = "v3_2_1"
@@ -16,7 +12,7 @@ var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
 	StoreUpgrades: store.StoreUpgrades{
-		Added:   []string{nftfactorytypes.StoreKey, buildertypes.StoreKey, ibchookstypes.StoreKey},
+		Added:   []string{},
 		Deleted: []string{},
 	},
 }
--- app/upgrades/v3.2.1/upgrades.go
+++ app/upgrades/v3.2.1/upgrades.go
@@ -7,13 +7,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
-
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
 	"github.com/UnUniFi/chain/app/keepers"
 	"github.com/UnUniFi/chain/app/upgrades"
-	yieldaggregatortypes "github.com/UnUniFi/chain/x/yieldaggregator/types"
 )
 
 func CreateUpgradeHandler(mm *module.Manager,
@@ -23,25 +20,17 @@ func CreateUpgradeHandler(mm *module.Manager,
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info(fmt.Sprintf("update start:%s", UpgradeName))
 
-		iyaParams := yieldaggregatortypes.Params{}
-		paramtypes.NewKeyTable().RegisterParamSet(&yieldaggregatortypes.Params{})
-		keepers.GetSubspace(yieldaggregatortypes.ModuleName).WithKeyTable(yieldaggregatortypes.ParamKeyTable()).GetParamSet(ctx, &iyaParams)
-
 		vm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
 		}
 
-		factoryParam, err := keepers.NftfactoryKeeper.GetParams(ctx)
+		iyaParams, err := keepers.YieldaggregatorKeeper.GetParams(ctx)
 		if err != nil {
 			return vm, err
 		}
-		factoryParam.ClassCreationFee = []sdk.Coin{}
-		factoryParam.FeeCollectorAddress = ""
-		_ = keepers.NftfactoryKeeper.SetParams(ctx, factoryParam)
-
 		iyaParams.FeeCollectorAddress = keepers.AccountKeeper.GetModuleAccount(ctx, authtypes.FeeCollectorName).GetAddress().String()
-		_ = keepers.YieldaggregatorKeeper.SetParams(ctx, &iyaParams)
+		_ = keepers.YieldaggregatorKeeper.SetParams(ctx, iyaParams)
 
 		return vm, nil
 	}
