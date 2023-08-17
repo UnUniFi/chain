e82894fcb3e08b7c624e36f89c912c438017030e
jununifi <jun.ununifi@outlook.com>
2023-08-17 18:15:37 +0800

    add cli commands for estimated redeem/mint amount, fix redeem amount estimator grpc

--- x/yieldaggregator/keeper/grpc_query_estimate_redeem_amount.go
+++ x/yieldaggregator/keeper/grpc_query_estimate_redeem_amount.go
@@ -3,13 +3,12 @@ package keeper
 import (
 	"context"
 
-	"github.com/UnUniFi/chain/x/yieldaggregator/types"
-	// "github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	// sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	// "github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+
+	osmomath "github.com/UnUniFi/chain/osmomath"
+	"github.com/UnUniFi/chain/x/yieldaggregator/types"
 )
 
 func (k Keeper) EstimateRedeemAmount(c context.Context, req *types.QueryEstimateRedeemAmountRequest) (*types.QueryEstimateRedeemAmountResponse, error) {
@@ -17,6 +16,10 @@ func (k Keeper) EstimateRedeemAmount(c context.Context, req *types.QueryEstimate
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 	ctx := sdk.UnwrapSDKContext(c)
+	params, err := k.GetParams(ctx)
+	if err != nil {
+		return nil, err
+	}
 
 	vault, found := k.GetVault(ctx, req.Id)
 	if !found {
@@ -26,9 +29,36 @@ func (k Keeper) EstimateRedeemAmount(c context.Context, req *types.QueryEstimate
 	if !ok {
 		return nil, types.ErrInvalidAmount
 	}
-	redeemAmount := k.EstimateRedeemAmountInternal(ctx, vault.Denom, vault.Id, burnAmount)
+	principal := k.EstimateRedeemAmountInternal(ctx, vault.Denom, vault.Id, burnAmount)
+
+	// Unstake funds from Strategy
+	amountToUnbond := principal.Amount
+
+	// implement fees on withdrawal
+	amountInVault := k.VaultWithdrawalAmount(ctx, vault)
+	amountUnbonding := k.VaultUnbondingAmountInStrategies(ctx, vault)
+
+	reserveMaintenanceRate := sdk.ZeroDec()
+	if amountInVault.GT(amountToUnbond) {
+		reserveMaintenanceRate = sdk.NewDecFromInt(amountInVault.Sub(amountToUnbond)).Quo(sdk.NewDecFromInt(amountInVault.Add(amountUnbonding)))
+	}
+
+	e := osmomath.NewDecWithPrec(2718281, 6) // 2.718281
+	withdrawFeeRate := osmomath.OneDec().
+		Quo(e.Power(osmomath.BigDecFromSDKDec(reserveMaintenanceRate).MulInt64(10))).
+		SDKDec()
+
+	withdrawFee := sdk.NewDecFromInt(principal.Amount).Mul(withdrawFeeRate).RoundInt()
+	withdrawAmount := principal.Amount.Sub(withdrawFee)
+
+	withdrawModuleCommissionFee := sdk.NewDecFromInt(withdrawAmount).Mul(params.CommissionRate).RoundInt()
+	withdrawVaultCommissionFee := sdk.NewDecFromInt(withdrawAmount).Mul(vault.WithdrawCommissionRate).RoundInt()
+	withdrawAmountWithoutCommission := withdrawAmount.Sub(withdrawModuleCommissionFee).Sub(withdrawVaultCommissionFee)
+	fee := withdrawModuleCommissionFee.Add(withdrawVaultCommissionFee)
 
 	return &types.QueryEstimateRedeemAmountResponse{
-		RedeemAmount: redeemAmount,
+		ShareAmount:  sdk.NewCoin(types.GetLPTokenDenom(vault.Id), burnAmount),
+		Fee:          sdk.NewCoin(principal.Denom, fee),
+		RedeemAmount: sdk.NewCoin(principal.Denom, withdrawAmountWithoutCommission),
 	}, nil
 }
