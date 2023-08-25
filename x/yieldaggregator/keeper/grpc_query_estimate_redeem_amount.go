3540738b44113b6ef5382eed63491a09af0cb7f9
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-08-25 22:03:51 +0900

    fix: query logic

--- x/yieldaggregator/keeper/grpc_query_estimate_redeem_amount.go
+++ x/yieldaggregator/keeper/grpc_query_estimate_redeem_amount.go
@@ -57,8 +57,9 @@ func (k Keeper) EstimateRedeemAmount(c context.Context, req *types.QueryEstimate
 	fee := withdrawFee.Add(withdrawModuleCommissionFee).Add(withdrawVaultCommissionFee)
 
 	return &types.QueryEstimateRedeemAmountResponse{
-		ShareAmount:  principal,
+		ShareAmount:  sdk.NewCoin(types.GetLPTokenDenom(vault.Id), burnAmount),
 		Fee:          sdk.NewCoin(principal.Denom, fee),
 		RedeemAmount: sdk.NewCoin(principal.Denom, withdrawAmountWithoutCommission),
+		TotalAmount:  principal,
 	}, nil
 }
