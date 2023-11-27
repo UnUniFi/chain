986bd26e89d33605d99cd0f0d1f95ff4fd184d99
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-11-27 18:06:59 +0900

    fix: delete send_enabled

--- app/upgrades/v4.0.1/upgrades.go
+++ app/upgrades/v4.0.1/upgrades.go
@@ -18,8 +18,6 @@ func CreateUpgradeHandler(mm *module.Manager,
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		ctx.Logger().Info(fmt.Sprintf("update start:%s", UpgradeName))
 
-		keepers.BankKeeper.SetSendEnabled(ctx, Denom, true)
-
 		result, err := BankSendList(ctx)
 		if err != nil {
 			panic(err)
@@ -28,8 +26,6 @@ func CreateUpgradeHandler(mm *module.Manager,
 		if err != nil {
 			panic(err)
 		}
-
-		keepers.BankKeeper.SetSendEnabled(ctx, Denom, false)
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
