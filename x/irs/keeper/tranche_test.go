97ee5687665de6ee3f2eb89a582df4e009aa34e2
Senna46 <29295263+Senna46@users.noreply.github.com>
2023-11-30 12:54:49 +0900

    feat: tranche

--- x/irs/keeper/tranche_test.go
+++ x/irs/keeper/tranche_test.go
@@ -88,3 +88,85 @@ func (suite *KeeperTestSuite) TestGetTranchesByStrategy() {
 	pools2 := suite.app.IrsKeeper.GetTranchesByStrategy(suite.ctx, address)
 	suite.Require().Equal(pools, pools2)
 }
+
+func (suite *KeeperTestSuite) TestGetAllTranchePool() {
+	pools := []types.TranchePool{
+		{
+			Id:               1,
+			StrategyContract: "address01",
+			StartTime:        1698796800,
+			Maturity:         1714521600,
+			SwapFee:          sdk.ZeroDec(),
+			ExitFee:          sdk.OneDec(),
+			TotalShares:      sdk.NewInt64Coin("uatom", 1000000),
+			PoolAssets: sdk.Coins{
+				sdk.NewInt64Coin("uatom", 1000000),
+				sdk.NewInt64Coin("uosmo", 1000000),
+			},
+		},
+		{
+			Id:               2,
+			StrategyContract: "address02",
+			StartTime:        1698796800,
+			Maturity:         1730419200,
+			SwapFee:          sdk.ZeroDec(),
+			ExitFee:          sdk.ZeroDec(),
+			TotalShares:      sdk.NewInt64Coin("uatom", 500000),
+			PoolAssets: sdk.Coins{
+				sdk.NewInt64Coin("uatom", 1000000),
+				sdk.NewInt64Coin("uosmo", 1000000),
+			},
+		},
+	}
+
+	for _, p := range pools {
+		suite.app.IrsKeeper.SetTranchePool(suite.ctx, p)
+	}
+	pools2 := suite.app.IrsKeeper.GetAllTranchePool(suite.ctx)
+	suite.Require().Equal(pools, pools2)
+}
+
+func (suite *KeeperTestSuite) TestGetLastTrancheId() {
+	id1 := uint64(1)
+	id2 := uint64(2)
+	pools := []types.TranchePool{
+		{
+			Id:               id1,
+			StrategyContract: "address01",
+			StartTime:        1698796800,
+			Maturity:         1714521600,
+			SwapFee:          sdk.ZeroDec(),
+			ExitFee:          sdk.OneDec(),
+			TotalShares:      sdk.NewInt64Coin("uatom", 1000000),
+			PoolAssets: sdk.Coins{
+				sdk.NewInt64Coin("uatom", 1000000),
+				sdk.NewInt64Coin("uosmo", 1000000),
+			},
+		},
+		{
+			Id:               id2,
+			StrategyContract: "address02",
+			StartTime:        1698796800,
+			Maturity:         1730419200,
+			SwapFee:          sdk.ZeroDec(),
+			ExitFee:          sdk.ZeroDec(),
+			TotalShares:      sdk.NewInt64Coin("uatom", 500000),
+			PoolAssets: sdk.Coins{
+				sdk.NewInt64Coin("uatom", 1000000),
+				sdk.NewInt64Coin("uosmo", 1000000),
+			},
+		},
+	}
+
+	for _, p := range pools {
+		suite.app.IrsKeeper.SetTranchePool(suite.ctx, p)
+	}
+	id := suite.app.IrsKeeper.GetLastTrancheId(suite.ctx)
+	suite.Require().Equal(id, id2)
+
+	for _, p := range pools {
+		suite.app.IrsKeeper.RemoveTranchePool(suite.ctx, p)
+	}
+	emptyId := suite.app.IrsKeeper.GetLastTrancheId(suite.ctx)
+	suite.Require().Equal(emptyId, uint64(0))
+}
