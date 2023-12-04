6908c3fbfaf6f23c201a8f6a5f113ef8e599888d
jununifi <jun.ununifi@outlook.com>
2023-12-04 22:09:09 +0800

    Add basic unit test for SwapExactAmountIn

--- x/irs/keeper/swap_test.go
+++ x/irs/keeper/swap_test.go
@@ -1,6 +1,12 @@
 package keeper_test
 
-// sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"github.com/cometbft/cometbft/crypto/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+
+	"github.com/UnUniFi/chain/x/irs/types"
+)
 
 // SwapUtToPt(ctx sdk.Context, sender sdk.AccAddress, pool types.TranchePool, tokenIn sdk.Coin) error {
 // SwapPtToUt(ctx sdk.Context, sender sdk.AccAddress, pool types.TranchePool, tokenIn sdk.Coin) error {
@@ -12,146 +18,160 @@ package keeper_test
 // 	tokenOut sdk.Coin,
 // ) error
 
-// func (s *KeeperTestSuite) TestSwapExactAmountIn() {
-// 	type param struct {
-// 		tokenIn           sdk.Coin
-// 		tokenOutDenom     string
-// 		tokenOutMinAmount sdk.Int
-// 		expectedTokenOut  sdk.Int
-// 	}
+func (s *KeeperTestSuite) TestSwapExactAmountIn() {
+	sender := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address().Bytes())
 
-// 	tests := []struct {
-// 		name  string
-// 		param param
-// 		swapFee sdk.Dec
-// 		expectPass                    bool
-// 	}{
-// 		{
-// 			name: "Proper swap",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("foo", sdk.NewInt(100000)),
-// 				tokenOutDenom:     "bar",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 				expectedTokenOut:  sdk.NewInt(49262),
-// 			},
-// 			expectPass: true,
-// 		},
-// 		{
-// 			name: "Proper swap2",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("bar", sdk.NewInt(2451783)),
-// 				tokenOutDenom:     "baz",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 				expectedTokenOut:  sdk.NewInt(1167843),
-// 			},
-// 			expectPass: true,
-// 		},
-// 		{
-// 			name: "boundary valid spread factor given (= 0.5 pool spread factor)",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("foo", sdk.NewInt(100000)),
-// 				tokenOutDenom:     "bar",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 				expectedTokenOut:  sdk.NewInt(46833),
-// 			},
-// 			swapFee:         sdk.MustNewDecFromStr("0.1"),
-// 			expectPass:                    true,
-// 		},
-// 		{
-// 			name: "invalid spread factor given (< 0.5 pool spread factor)",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("foo", sdk.NewInt(100000)),
-// 				tokenOutDenom:     "bar",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 				expectedTokenOut:  sdk.NewInt(49262),
-// 			},
-// 			swapFee:         sdk.MustNewDecFromStr("0.1"),
-// 			expectPass:                    false,
-// 		},
-// 		{
-// 			name: "out is lesser than min amount",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("bar", sdk.NewInt(2451783)),
-// 				tokenOutDenom:     "baz",
-// 				tokenOutMinAmount: sdk.NewInt(9000000),
-// 			},
-// 			expectPass: false,
-// 		},
-// 		{
-// 			name: "in and out denom are same",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("bar", sdk.NewInt(2451783)),
-// 				tokenOutDenom:     "bar",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 			},
-// 			expectPass: false,
-// 		},
-// 		{
-// 			name: "unknown in denom",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("bara", sdk.NewInt(2451783)),
-// 				tokenOutDenom:     "bar",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 			},
-// 			expectPass: false,
-// 		},
-// 		{
-// 			name: "unknown out denom",
-// 			param: param{
-// 				tokenIn:           sdk.NewCoin("bar", sdk.NewInt(2451783)),
-// 				tokenOutDenom:     "bara",
-// 				tokenOutMinAmount: sdk.NewInt(1),
-// 			},
-// 			expectPass: false,
-// 		},
-// 	}
+	strategyContract := sdk.AccAddress("strategy_contract_address")
+	ut := "uatom"
+	pt := types.PtDenom(types.TranchePool{Id: 1})
+	lp := types.LsDenom(types.TranchePool{Id: 1})
 
-// 	for _, test := range tests {
-// 		test := test
-// 		s.Run(test.name, func() {
-// 			// Init suite for each test.
-// 			s.SetupTest()
-// 			swapFee := sdk.ZeroDec()
-// 			if !test.swapFee.IsNil() {
-// 				swapFee = test.swapFee
-// 			}
-// 			poolId := s.PrepareBalancerPoolWithPoolParams(balancer.PoolParams{
-// 				SwapFee: swapFee,
-// 				ExitFee: sdk.ZeroDec(),
-// 			})
-// 			keeper := s.App.IrsKeeper
-// 			ctx := s.ctx
-// 			pool, err := s.App.IrsKeeper.GetTranchePool(ctx, poolId)
-// 			s.NoError(err)
+	ptUt5k := sdk.NewCoins(sdk.NewCoin(ut, sdk.NewInt(5000)), sdk.NewCoin(pt, sdk.NewInt(5000))).Sort()
 
-// 			if test.expectPass {
-// 				spotPriceBefore, err := keeper.CalculateSpotPrice(ctx, poolId, test.param.tokenIn.Denom, test.param.tokenOutDenom)
-// 				s.NoError(err, "test: %v", test.name)
+	type param struct {
+		tokenIn           sdk.Coin
+		tokenOutDenom     string
+		tokenOutMinAmount sdk.Int
+		expectedTokenOut  sdk.Int
+	}
 
-// 				prevGasConsumed := s.ctx.GasMeter().GasConsumed()
-// 				tokenOutAmount, err := keeper.SwapExactAmountIn(ctx, s.TestAccs[0], pool, test.param.tokenIn, test.param.tokenOutDenom, test.param.tokenOutMinAmount, spreadFactor)
-// 				s.NoError(err, "test: %v", test.name)
-// 				s.Require().Equal(test.param.expectedTokenOut.String(), tokenOutAmount.String())
-// 				gasConsumedForSwap := s.ctx.GasMeter().GasConsumed() - prevGasConsumed
-// 				// We consume `types.GasFeeForSwap` directly, so the extra I/O operation mean we end up consuming more.
-// 				s.Assert().Greater(gasConsumedForSwap, uint64(types.BalancerGasFeeForSwap))
+	tests := []struct {
+		name       string
+		param      param
+		swapFee    sdk.Dec
+		expectPass bool
+	}{
+		{
+			name: "Proper swap",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(100000)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(1),
+				expectedTokenOut:  sdk.NewInt(49262),
+			},
+			expectPass: true,
+		},
+		{
+			name: "Proper swap2",
+			param: param{
+				tokenIn:           sdk.NewCoin(ut, sdk.NewInt(2451783)),
+				tokenOutDenom:     pt,
+				tokenOutMinAmount: sdk.NewInt(1),
+				expectedTokenOut:  sdk.NewInt(1167843),
+			},
+			expectPass: true,
+		},
+		{
+			name: "boundary valid spread factor given (= 0.5 pool spread factor)",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(100000)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(1),
+				expectedTokenOut:  sdk.NewInt(46833),
+			},
+			swapFee:    sdk.MustNewDecFromStr("0.1"),
+			expectPass: true,
+		},
+		{
+			name: "invalid spread factor given (< 0.5 pool spread factor)",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(100000)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(1),
+				expectedTokenOut:  sdk.NewInt(49262),
+			},
+			swapFee:    sdk.MustNewDecFromStr("0.1"),
+			expectPass: false,
+		},
+		{
+			name: "out is lesser than min amount",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(2451783)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(9000000),
+			},
+			expectPass: false,
+		},
+		{
+			name: "in and out denom are same",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(2451783)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(1),
+			},
+			expectPass: false,
+		},
+		{
+			name: "unknown in denom",
+			param: param{
+				tokenIn:           sdk.NewCoin("unknown", sdk.NewInt(2451783)),
+				tokenOutDenom:     ut,
+				tokenOutMinAmount: sdk.NewInt(1),
+			},
+			expectPass: false,
+		},
+		{
+			name: "unknown out denom",
+			param: param{
+				tokenIn:           sdk.NewCoin(pt, sdk.NewInt(2451783)),
+				tokenOutDenom:     "unknown",
+				tokenOutMinAmount: sdk.NewInt(1),
+			},
+			expectPass: false,
+		},
+	}
 
-// 				s.AssertEventEmitted(ctx, types.TypeEvtTokenSwapped, 1)
+	for _, test := range tests {
+		test := test
+		s.Run(test.name, func() {
+			// Init suite for each test.
+			s.SetupTest()
+			ctx := s.ctx
+			irsKeeper := s.app.IrsKeeper
+			bankKeeper := s.app.BankKeeper
 
-// 				spotPriceAfter, err := keeper.CalculateSpotPrice(ctx, poolId, test.param.tokenIn.Denom, test.param.tokenOutDenom)
-// 				s.NoError(err, "test: %v", test.name)
+			sharesRequested := types.OneShare
+			existingShares := sdk.ZeroInt()
+			poolTokens := sdk.Coins{}
 
-// 				if !test.swapFee.IsNil() {
-// 					return
-// 				}
+			// Create the tranche pool at first
+			tranchePool := types.TranchePool{
+				Id:               1,
+				StrategyContract: strategyContract.String(),
+				StartTime:        uint64(ctx.BlockTime().Unix()),
+				Maturity:         86400 * 180,
+				SwapFee:          sdk.NewDecWithPrec(3, 3), // 0.3%
+				ExitFee:          sdk.ZeroDec(),
+				TotalShares:      sdk.NewCoin(lp, existingShares),
+				PoolAssets:       poolTokens,
+			}
+			irsKeeper.SetTranchePool(ctx, tranchePool)
 
-// 				// Ratio of the token out should be between the before spot price and after spot price.
-// 				tradeAvgPrice := test.param.tokenIn.Amount.ToDec().Quo(tokenOutAmount.ToDec())
-// 				s.True(tradeAvgPrice.GT(spotPriceBefore) && tradeAvgPrice.LT(spotPriceAfter), "test: %v", test.name)
-// 			} else {
-// 				_, err := keeper.SwapExactAmountIn(ctx, s.TestAccs[0], pool, test.param.tokenIn, test.param.tokenOutDenom, test.param.tokenOutMinAmount, spreadFactor)
-// 				s.Error(err, "test: %v", test.name)
-// 			}
-// 		})
-// 	}
-// }
+			coins := sdk.Coins{sdk.NewInt64Coin(ut, 1000000000)}.Add(sdk.NewInt64Coin(pt, 1000000000))
+			err := bankKeeper.MintCoins(ctx, minttypes.ModuleName, coins)
+			s.Require().NoError(err)
+
+			err = bankKeeper.SendCoinsFromModuleToAccount(ctx, minttypes.ModuleName, sender, coins)
+			s.Require().NoError(err)
+
+			_, _, err = irsKeeper.DepositToLiquidityPool(ctx, sender, tranchePool.Id, sharesRequested, ptUt5k)
+			s.Require().NoError(err)
+
+			tranchePool, found := irsKeeper.GetTranchePool(ctx, tranchePool.Id)
+			s.Require().True(found)
+
+			if test.expectPass {
+				tokenOutAmount, err := irsKeeper.SwapExactAmountIn(ctx, sender, tranchePool, test.param.tokenIn, test.param.tokenOutDenom, test.param.tokenOutMinAmount, test.swapFee)
+				s.NoError(err, "test: %v", test.name)
+				s.Require().Equal(test.param.expectedTokenOut.String(), tokenOutAmount.String())
+
+				if !test.swapFee.IsNil() {
+					return
+				}
+			} else {
+				_, err := irsKeeper.SwapExactAmountIn(ctx, sender, tranchePool, test.param.tokenIn, test.param.tokenOutDenom, test.param.tokenOutMinAmount, test.swapFee)
+				s.Error(err, "test: %v", test.name)
+			}
+		})
+	}
+}
